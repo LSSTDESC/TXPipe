e59fc61967413de3ff8349b6697d2b2b1904cdbb
Joe Zuntz <joezuntz@googlemail.com>
2018-10-24 13:18:47 +0100

    Revert "change unseen pixel values to zero, instead of nans"
    This reverts commit 73e10dc485cf4acf715e9bfdac203fd386d39645.

--- txpipe/utils/stats.py
+++ txpipe/utils/stats.py
@@ -374,8 +374,8 @@ class ParallelStatsCalculator:
         variance = S2 / T
         mean[w] = S[w] / T[w]
         count = T
-        variance[count<2] = 0
-        mean[count<1] = 0
+        variance[count<2] = np.nan
+        mean[count<1] = np.nan
         return count, mean, variance
 
 
