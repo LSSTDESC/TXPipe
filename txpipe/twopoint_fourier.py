67306a3e19aefc72e788b50ed41e8508bb025e79
Joe Zuntz <joezuntz@googlemail.com>
2019-02-07 19:09:21 +0000

    Add noise estimation to fourier

--- txpipe/twopoint_fourier.py
+++ txpipe/twopoint_fourier.py
@@ -4,6 +4,8 @@ import numpy as np
 import collections
 from .utils import choose_pixelization, HealpixScheme, GnomonicPixelScheme, ParallelStatsCalculator
 import sys
+import warnings
+
 
 # Using the same convention as in twopoint.py
 SHEAR_SHEAR = 0
@@ -36,6 +38,8 @@ class TXTwoPointFourier(PipelineStage):
     inputs = [
         ('photoz_stack', HDFFile),  # Photoz stack
         ('diagnostic_maps', DiagnosticMaps),
+        ('fiducial_cosmology', YamlFile),  # For the cosmological parameters
+        ('tomography_catalog', TomographyCatalog),  # For density info
     ]
     outputs = [
         ('twopoint_data', SACCFile)
@@ -68,9 +72,24 @@ class TXTwoPointFourier(PipelineStage):
         # This will be used for parallelization.
         calcs = self.select_calculations(nbin_source, nbin_lens)
 
+
+        # Load in the per-bin auto-correlation noise levels and 
+        # mean response values
+        noise, mean_R = self.load_tomographic_quantities(nbin_source, nbin_lens, f_sky)
+
+        # Load the n(z) values, which are both saved in the output
+        # file alongside the spectra, and used to calcualate the
+        # fiducial theory C_ell
+        tracers = self.load_tracers(nbin_source, nbin_lens)
+
+        # This is needed in the deprojection calculation
+        theory_cl = self.fiducial_theory(tracers, f_d, f_wl, nbin_source, nbin_lens)
+
         # Binning scheme, currently chosen from the geometry.
         # TODO: set ell binning from config
         ell_bins = self.choose_ell_bins(pixel_scheme, f_sky)
+
+        # If we are rank zero print out some info
         if self.rank==0:
             nband = ell_bins.get_n_bands()
             ell_effs = ell_bins.get_effective_ells()
@@ -80,6 +99,9 @@ class TXTwoPointFourier(PipelineStage):
                 lmin = ell_bins.get_ell_min(i)
                 lmax = ell_bins.get_ell_max(i)
                 print(f"    {leff:.0f}    ({lmin:.0f} - {lmax:.0f})")
+
+
+
 
         # Namaster uses workspaces, which we re-use between
         # bins
@@ -92,7 +114,7 @@ class TXTwoPointFourier(PipelineStage):
         # as it's not dynamic, just a round-robin assignment.
         for i, j, k in self.split_tasks_by_rank(calcs):
             self.compute_power_spectra(
-                pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins)
+                pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins, noise, theory_cl)
 
         if self.rank==0:
             print(f"Collecting results together")
@@ -101,7 +123,7 @@ class TXTwoPointFourier(PipelineStage):
 
         # Write the collect results out to HDF5.
         if self.rank == 0:
-            self.save_power_spectra(nbin_source, nbin_lens)
+            self.save_power_spectra(tracers, nbin_source, nbin_lens)
             print("Saved power spectra")
 
 
@@ -330,7 +352,7 @@ class TXTwoPointFourier(PipelineStage):
 
         return calcs
 
-    def compute_power_spectra(self, pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins):
+    def compute_power_spectra(self, pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins, noise, cl_theory):
         # Compute power spectra
         # TODO: now all possible auto- and cross-correlation are computed.
         #      This should be tunable.
@@ -340,12 +362,20 @@ class TXTwoPointFourier(PipelineStage):
         type_name = NAMES[k]
         print(f"Process {self.rank} calcluating {type_name} spectrum for bin pair {i},{i}")
         sys.stdout.flush()
+
+        # We need the theory spectrum for this pair
+        theory = cl_theory[(i,j,k)]
+
+
+
         if k == SHEAR_SHEAR:
             ls = ell_bins.get_effective_ells()
             # Top-hat window functions
             win = [ell_bins.get_window(b) for b,l  in enumerate(ls)]
+            cl_noise = self.compute_noise(i,j,k,w22,noise)
+            cl_guess = [theory, np.zeros_like(theory), np.zeros_like(theory), np.zeros_like(theory)]
             c = self.compute_one_spectrum(
-                pixel_scheme, w22, f_wl[i], f_wl[j], ell_bins)
+                pixel_scheme, w22, f_wl[i], f_wl[j], ell_bins, cl_noise, cl_guess)
             c_EE = c[0]
             c_BB = c[3]
             self.results.append(Measurement('CEE', ls, c_EE, win, i, j))
@@ -354,35 +384,103 @@ class TXTwoPointFourier(PipelineStage):
         if k == POS_POS:
             ls = ell_bins.get_effective_ells()
             win = [ell_bins.get_window(b) for b,l  in enumerate(ls)]
+            cl_noise = self.compute_noise(i,j,k,w00,noise)
+            cl_guess = [theory]
             c = self.compute_one_spectrum(
-                pixel_scheme, w00, f_d[i], f_d[j], ell_bins)
+                pixel_scheme, w00, f_d[i], f_d[j], ell_bins, cl_noise, cl_guess)
             c_dd = c[0]
             self.results.append(Measurement('Cdd', ls, c_dd, win, i, j))
 
         if k == SHEAR_POS:
             ls = ell_bins.get_effective_ells()
             win = [ell_bins.get_window(b) for b,l  in enumerate(ls)]
+            cl_noise = self.compute_noise(i,j,k,w02,noise)
+            cl_guess = [theory, np.zeros_like(theory)]
+
             c = self.compute_one_spectrum(
-                pixel_scheme, w02, f_wl[i], f_d[j], ell_bins)
+                pixel_scheme, w02, f_wl[i], f_d[j], ell_bins, cl_noise, cl_guess)
             c_dE = c[0]
             c_dB = c[1]
             self.results.append(Measurement('CdE', ls, c_dE, win, i, j))
             self.results.append(Measurement('CdB', ls, c_dB, win, i, j))
 
+    def compute_noise(self, i, j, k, w, noise):
+        # No noise contribution from cross-correlations
+        if (i!=j) or (k==SHEAR_POS):
+            return None
+        print("x")
+        # We loaded in sigma_e and the densities
+        # earlier on and put them in the noise dictionary
+        noise_level = noise[(i,k)]
 
-    def compute_one_spectrum(self, pixel_scheme, w, f1, f2, ell_bins):
+        # ell-by-ell noise level of the right size
+        N1 = np.ones(w.wsp.lmax + 1) * noise[(i,k)]
+
+        # return N1
+
+        # # Need the same noise for EE, BB, EB, BE
+        # # or PE, PB
+        if k==SHEAR_SHEAR:
+            N2 = [N1, N1, N1, N1]
+        elif k==SHEAR_POS:
+            N2 = [N1, N1]
+        else:
+            N2 = [N1]
+        return N2
+
+        # # Run the same decoupling process that we will use
+        # # on the full spectra
+        # N_b = w.decouple_cell(N2)[0]
+
+        # return N_b
+
+
+    def compute_one_spectrum(self, pixel_scheme, w, f1, f2, ell_bins, cl_noise, theory):
         import pymaster as nmt
+
+
+
         if pixel_scheme.name == 'healpix':
             # correlates two fields f1 and f2 and returns an array of coupled
             # power spectra
             coupled_c_ell = nmt.compute_coupled_cell(f1, f2)
+            # Compute 
+            cl_bias = nmt.deprojection_bias(f1, f2, theory)
+
         elif pixel_scheme.name == 'gnomonic':
             coupled_c_ell = nmt.compute_coupled_cell_flat(f1, f2, ell_bins)
+            ell_eff = ell_bins.get_effective_ells()
+            cl_bias = nmt.deprojection_bias_flat(f1, f2, ell_bins, ell_eff, cl_theory)
 
-        c_ell = w.decouple_cell(coupled_c_ell)
+        c_ell = w.decouple_cell(coupled_c_ell, cl_noise=cl_noise, cl_bias=cl_bias)
         return c_ell
 
-    def save_power_spectra(self, nbin_source, nbin_lens):
+    def load_tomographic_quantities(self, nbin_source, nbin_lens, f_sky):
+        tomo = self.open_input('tomography_catalog')
+        sigma_e = tomo['tomography/sigma_e'][:]
+        mean_R = tomo['multiplicative_bias/mean_R'][:]
+        N_eff = tomo['tomography/N_eff'][:]
+        lens_counts = tomo['tomography/lens_counts'][:]
+        tomo.close()
+
+        area = 4*np.pi*f_sky
+
+        n_eff = N_eff / area
+        n_lens = lens_counts / area
+
+        noise = {}
+        for i in range(nbin_source):
+            noise[(i,SHEAR_SHEAR)] = sigma_e[i]**2 / n_eff[i]
+        for i in range(nbin_lens):
+            noise[(i,POS_POS)] = 1.0 / n_lens[i]
+
+        warnings.warn("Using unweighted lens samples here")
+
+        return noise, mean_R
+
+
+
+    def load_tracers(self, nbin_source, nbin_lens):
         import sacc
         f = self.open_input('photoz_stack')
 
@@ -391,7 +489,7 @@ class TXTwoPointFourier(PipelineStage):
         for i in range(nbin_source):
             z = f['n_of_z/source/z'].value
             Nz = f[f'n_of_z/source/bin_{i}'].value
-            T = sacc.Tracer(f"LSST source_{i}", b"spin0", z, Nz, exp_sample=b"LSST-source")
+            T = sacc.Tracer(f"LSST source_{i}", b"spin2", z, Nz, exp_sample=b"LSST-source")
             tracers.append(T)
 
         for i in range(nbin_lens):
@@ -399,6 +497,75 @@ class TXTwoPointFourier(PipelineStage):
             Nz = f[f'n_of_z/lens/bin_{i}'].value
             T = sacc.Tracer(f"LSST lens_{i}", b"spin0", z, Nz, exp_sample=b"LSST-lens")
             tracers.append(T)
+
+        return tracers
+
+    def fiducial_theory(self, tracers, f_d, f_wl, nbin_source, nbin_lens):
+        import pyccl as ccl
+
+        filename = self.get_input('fiducial_cosmology')
+        cosmo = ccl.Cosmology.read_yaml(filename)
+
+        # We will need the theory C_ell in a continuum up until
+        # the full ell_max, because we will need a weighted sum
+        # over the values
+        ell_max = f_d[0].fl.lmax
+        ell = np.arange(ell_max+1, dtype=int)
+
+        # Convert from SACC tracers (which just store N(z))
+        # to CCL tracers (which also have cosmology info in them).
+        CTracers = {}
+
+        # Lensing tracers - need to think a little more about
+        # the fiducial intrinsic alignment here
+        for i in range(nbin_source):
+            x = tracers[i]
+            tag = ('S', i)
+            CTracers[tag] = ccl.WeakLensingTracer(cosmo, dndz=(x.z, x.Nz))
+        # Position tracers - even more important to think about fiducial biases
+        # here - these will be very very wrong otherwise!
+        # Important enough that I'll put in a warning.
+        warnings.warn("Not using galaxy bias in fiducial theory density spectra")
+
+        for i in range(nbin_lens):
+            x = tracers[i + nbin_source]
+            tag = ('P', i) 
+            b = np.ones_like(x.z)
+            CTracers[tag] = ccl.NumberCountsTracer(cosmo, dndz=(x.z, x.Nz),
+                                        has_rsd=False, bias=(x.z,b))
+
+        # Use CCL to actually calculate the C_ell values for the different cases
+        theory_cl = {}
+        k = SHEAR_SHEAR
+        for i in range(nbin_source):
+            for j in range(i+1):
+                Ti = CTracers[('S',i)]
+                Tj = CTracers[('S',j)]
+                # The full theory C_ell over the range 0..ellmax
+                theory_cl [(i,j,k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
+                
+
+        # The same for the galaxy galaxy-lensing cross-correlation
+        k = SHEAR_POS
+        for i in range(nbin_source):
+            for j in range(nbin_lens):
+                Ti = CTracers[('S',i)]
+                Tj = CTracers[('P',j)]
+                theory_cl [(i,j,k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
+
+        # And finally for the density correlations
+        k = POS_POS
+        for i in range(nbin_lens):
+            for j in range(i+1):
+                Ti = CTracers[('P',i)]
+                Tj = CTracers[('P',j)]
+                theory_cl [(i,j,k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
+
+        return theory_cl
+
+
+    def save_power_spectra(self, tracers, nbin_source, nbin_lens):
+        import sacc
 
         fields = ['corr_type', 'l', 'value', 'i', 'j', 'win']
         output = {f: list() for f in fields}
--- txpipe/utils/nmt_utils.py
+++ txpipe/utils/nmt_utils.py
@@ -2,6 +2,7 @@ import pymaster as nmt
 import numpy as np
 
 class MyNmtBinFlat(nmt.NmtBinFlat):
+    is_flat = True
     def __init__(self, l0, lf):
         super().__init__(l0, lf)
         self.ell_min = l0
@@ -16,7 +17,15 @@ class MyNmtBinFlat(nmt.NmtBinFlat):
     def get_ell_max(self, b):
         return self.ell_max[b]
 
+    def is_flat(self):
+        return True
+
+    def apply_window(self, b, c_ell):
+        b0, b1 = self.get_window(b)
+        return c_ell[b0:b1+1].mean()
+
 class MyNmtBin(nmt.NmtBin):
+    is_flat = False
     def get_window(self, b):
         ls = self.get_ell_list(b)
         w = self.get_weight_list(b)
@@ -27,3 +36,10 @@ class MyNmtBin(nmt.NmtBin):
 
     def get_ell_max(self, b):
         return self.get_ell_list(b)[-1]
+
+    def is_flat(self):
+        return False
+
+    def apply_window(self, b, c_ell):
+        ell, weight = self.get_window(b)
+        return (c_ell[ell]*weight).sum() / weight.sum()
