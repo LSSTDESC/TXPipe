7b25e209130606e8a5e847421e567d3e408c8bb1
[Emily PhillipsLongley] <[elp25@duke.edu]>
2019-03-01 16:38:54 -0800

    Added seperate outputs for each of the cases.

--- txpipe/covariance.py
+++ txpipe/covariance.py
@@ -31,19 +31,34 @@ class TXFourierGaussianCovariance(PipelineStage):
         # read the fiducial cosmology
         cosmo = self.read_cosmology()
 
-        # read binning
-        binning_info, data_vector, ells = self.read_sacc()
-
         # read the n(z) and f_sky from the source summary stats
         nz, n_eff, sigma_e, fsky, N_tomo_bins = self.read_number_statistics()
 
         # calculate the overall total C_ell values, including noise
-        theory_c_ell = self.compute_theory_c_ell(cosmo, nz, binning_info)
-        noise_c_ell = self.compute_noise_c_ell(n_eff, sigma_e, binning_info)
+        theory_c_ell_lensing, theory_c_ell_galaxy_galaxy, theory_c_ell_clustering = self.compute_theory_c_ell(cosmo, nz, binning_info)
+        noise_c_ell_lensing, noise_c_ell_galaxy_galaxy, noise_c_ell_clustering = self.compute_noise_c_ell(n_eff, sigma_e, binning_info)
+
+        # read the lensing binning
+        binning_info_lensing, data_vector_lensing, ells_lensing = self.read_sacc('Cll')
+
+        # compute the lensing covariance
+        C_lensing = self.compute_covariance_lensing(binning_info_lensing, theory_c_ell_lensing, noise_c_ell_lensing, fsky)
+        self.save_outputs_firecrown(C_lensing,data_vector_lensing,ells_lensing,nz,binning_info_lensing)
+
+        # read galaxy galaxy binning
+        binning_info_galaxy_galaxy, data_vector_galaxy_galaxy, ells_galaxy_galaxy = self.read_sacc('Cdl')
+
+        # compute galaxy galaxy covariance
+        C_galaxy_galaxy = self.compute_covariance_clustering(binning_info_galaxy_galaxy, theory_c_ell_galaxy_galaxy, noise_c_ell_galaxy_galaxy, fsky)
+        self.save_outputs_firecrown(C_galaxy_galaxy,data_vector_galaxy_galaxy,ells_galaxy_galaxy,nz,binning_info_galaxy_galaxy)
+
+        # read binning
+        binning_info_clustering, data_vector_clustering, ells_clustering = self.read_sacc('Cdd')
 
         # compute covariance
-        C = self.compute_covariance(binning_info, theory_c_ell, noise_c_ell, fsky)
-        self.save_outputs_firecrown(C,data_vector,ells,nz,binning_info)
+        C_clustering = self.compute_covariance_clustering(binning_info, theory_c_ell_clustering, noise_c_ell_clustering, fsky)
+        self.save_outputs_firecrown(C_clustering,data_vector_clustering,ells_clustering,nz,binning_info_clustering)
+
 
     def read_cosmology(self):
         import pyccl as ccl
@@ -54,20 +69,20 @@ class TXFourierGaussianCovariance(PipelineStage):
         print(cosmo)
         return cosmo
 
-    def read_sacc(self):
+    def read_sacc(self,code):
         import sacc
         f = self.get_input('twopoint_data_fourier')
         sacc_data = sacc.SACC.loadFromHDF(f)
 
         codes = {
             'Cll' : ('E','E'),
-            #'Cdd' : ('P','P'),
-            #'Cdl' : ('P','S'),
+            'Cdd' : ('P','P'),
+            'Cdl' : ('P','S'),
             }
 
         binning = {quant: {} for quant in codes}
 
-        for quant, (code1,code2) in codes.items():
+        for quant, (code1,code2) in [codes[code]]:
             bin_pairs = sacc_data.binning.get_bin_pairs(code1,code2)
             for (b1, b2) in bin_pairs:
                 angle = sacc_data.binning.get_angle(code1, code2, b1, b2)
@@ -117,7 +132,7 @@ class TXFourierGaussianCovariance(PipelineStage):
         # Use the cosmology object to calculate C_ell values
         import pyccl as ccl
 
-        theory_c_ell = {}
+        theory_c_ell_lensing = {}
         ell=binning['Cll'][0,0]
         z = nz.get('z')
 
@@ -126,30 +141,68 @@ class TXFourierGaussianCovariance(PipelineStage):
             nz_2 = nz.get('bin_' + str(key[1]))
             tracer1 = ccl.WeakLensingTracer(cosmo, dndz=(z, nz_1))
             tracer2 = ccl.WeakLensingTracer(cosmo, dndz=(z, nz_2))
-            theory_c_ell[str(key[0]) + str(key[1])] = ccl.angular_cl(cosmo, tracer1, tracer2, ell)
+            theory_c_ell_lensing[str(key[0]) + str(key[1])] = ccl.angular_cl(cosmo, tracer1, tracer2, ell)
 
-        print('THEORY_Cl:')
-        print(theory_c_ell)
-        return theory_c_ell
+        theory_c_ell_clustering = {}
+        ell=binning['Cdd'][0,0]
+        z = nz.get('z')
+        b = 1.5 # Need to update this to use a value that makes sense for the BOSS sample
+
+        for key in binning['Cdd']:
+            nz_1 = nz.get('bin_'+ str(key[0]))
+            nz_2 = nz.get('bin_' + str(key[1]))
+            tracer1 = ccl.NumberCountsTracer(cosmo, dndz=(z,dNdz), bias=(z,b))
+            tracer2 = ccl.NumberCountsTracer(cosmo, dndz=(z,dNdz), bias=(z,b))
+            theory_c_ell_clustering[str(key[0]) + str(key[1])] = ccl.angular_cl(cosmo, tracer1, tracer2, ell)
+
+
+        theory_c_ell_galaxy_galaxy = {}
+        ell=binning['Cdl'][0,0]
+        z = nz.get('z')
+        b = 1.5*np.ones(len(z)) #Setting bias to 1.5 for now
+
+        for key in binning['Cdl']:
+            nz_1 = nz.get('bin_'+ str(key[0]))
+            nz_2 = nz.get('bin_' + str(key[1]))
+            tracer1 = ccl.NumberCountsTracer(cosmo, dndz=(z,dNdz), bias=(z,b))
+            tracer2 = ccl.WeakLensingTracer(cosmo, dndz=(z, nz_2))
+            theory_c_ell_galaxy_galaxy[str(key[0]) + str(key[1])] = ccl.angular_cl(cosmo, tracer1, tracer2, ell)
+
+        return theory_c_ell_lensing, theory_c_ell_galaxy_galaxy, theory_c_ell_clustering
 
 
     def compute_noise_c_ell(self, n_eff, sigma_e, binning):
         #avg number of galaxies in a zbin
-        noise_c_ell = {}
+        noise_c_ell_lensing = {}
+        noise_c_ell_clustering = {}
+        noise_c_ell_galaxy_galaxy = {}
         ell=binning['Cll'][0,0]
 
         for key in binning['Cll']:
             if key[0]==key[1]:
-                noise_c_ell[str(key[0]) + str(key[1])] = np.ones(len(ell))*(sigma_e[key[0]]**2/n_eff[key[0]])
+                noise_c_ell_lensing[str(key[0]) + str(key[1])] = np.ones(len(ell))*(sigma_e[key[0]]**2/n_eff[key[0]])
             else:
-                noise_c_ell[str(key[0]) + str(key[1])] = np.zeros(len(ell))
+                noise_c_ell_lensing[str(key[0]) + str(key[1])] = np.zeros(len(ell))
 
-        print('NOISE_Cl:')
-        print(noise_c_ell)
-        return noise_c_ell
+        ell=binning['Cdl'][0,0]
+
+        for key in binning['Cdl']:
+            if key[0]==key[1]:
+                noise_c_ell_galaxy_galaxy[str(key[0]) + str(key[1])] = np.ones(len(ell))*(sigma_e[key[0]]**2/n_eff[key[0]])
+            else:
+                noise_c_ell_galaxy_galaxy[str(key[0]) + str(key[1])] = np.zeros(len(ell))
+
+        ell=binning['Cdd'][0,0]
+
+        for key in binning['Cdd']:
+            if key[0]==key[1]:
+                noise_c_ell_clustering[str(key[0]) + str(key[1])] = np.ones(len(ell))
+            else:
+                noise_c_ell_clustering[str(key[0]) + str(key[1])] = np.zeros(len(ell))
+        return noise_c_ell_lensing, noise_c_ell_galaxy_galaxy, noise_c_ell_clustering
 
 
-    def compute_covariance(self, binning, theory_c_ell, noise_c_ell, fsky):
+    def compute_covariance_lensing(self, binning, theory_c_ell, noise_c_ell, fsky):
         ell=binning['Cll'][0,0]
         delta_ell=ell[1]-ell[0] #not in general equal, this needs to be improved
 
@@ -201,6 +254,110 @@ class TXFourierGaussianCovariance(PipelineStage):
                     indexcol = 0
         return cov
 
+    def compute_covariance_galaxy_galaxy(self, binning, theory_c_ell, noise_c_ell, fsky):
+        ell=binning['Cdl'][0,0]
+        delta_ell=ell[1]-ell[0] #not in general equal, this needs to be improved
+
+        obs_c_ell = {}
+        for key in binning['Cdl']:
+            obs_c_ell[str(key[0]) + str(key[1])] = theory_c_ell[str(key[0]) + str(key[1])] + noise_c_ell[str(key[0]) + str(key[1])]
+
+        def switch_keys(bin_1, bin_2, obs_c_ell):
+            if str(bin_1) + str(bin_2) in theory_c_ell.keys():
+                obs_c_ell_xy = obs_c_ell.get(str(bin_1) + str(bin_2))
+                return obs_c_ell_xy
+            else:
+                obs_c_ell_yx = obs_c_ell.get(str(bin_2) + str(bin_1))
+                return obs_c_ell_yx
+
+
+        indexrow = 0
+        indexcol = 0
+        cov=np.zeros((len(binning['Cdl'])*len(ell),len(binning['Cdl'])*len(ell)))
+
+        for key_row in binning['Cdl']:
+            for key_col in binning['Cdl']:
+
+                i = key_row[0]
+                j = key_row[1]
+                m = key_col[0]
+                n = key_col[1]
+
+                obs_c_ell_im = switch_keys(str(i), str(m), obs_c_ell)
+                obs_c_ell_jn = switch_keys(str(j), str(n), obs_c_ell)
+                obs_c_ell_in = switch_keys(str(i), str(n), obs_c_ell)
+                obs_c_ell_jm = switch_keys(str(j), str(m), obs_c_ell)
+
+                prefactor = 1./((2*ell+1)*delta_ell*fsky)
+
+                mini_cov = np.zeros((len(ell),len(ell)))
+                for a in range(len(ell)):
+                    for b in range(len(ell)):
+                        if a==b:
+                            mini_cov[a][b] = obs_c_ell_im[a]*obs_c_ell_jn[b] + obs_c_ell_in[a]*obs_c_ell_jm[b]
+
+                cov[indexrow*len(ell):indexrow*len(ell)+len(ell),indexcol*len(ell):indexcol*len(ell)+len(ell)] = prefactor*mini_cov
+                print(prefactor)
+                print(mini_cov)
+
+                indexcol += 1
+                if indexcol == 10:
+                    indexrow += 1
+                    indexcol = 0
+        return cov
+
+    def compute_covariance_clustering(self, binning, theory_c_ell, noise_c_ell, fsky):
+        ell=binning['Cdd'][0,0]
+        delta_ell=ell[1]-ell[0] #not in general equal, this needs to be improved
+
+        obs_c_ell = {}
+        for key in binning['Cdd']:
+            obs_c_ell[str(key[0]) + str(key[1])] = theory_c_ell[str(key[0]) + str(key[1])] + noise_c_ell[str(key[0]) + str(key[1])]
+
+        def switch_keys(bin_1, bin_2, obs_c_ell):
+            if str(bin_1) + str(bin_2) in theory_c_ell.keys():
+                obs_c_ell_xy = obs_c_ell.get(str(bin_1) + str(bin_2))
+                return obs_c_ell_xy
+            else:
+                obs_c_ell_yx = obs_c_ell.get(str(bin_2) + str(bin_1))
+                return obs_c_ell_yx
+
+
+        indexrow = 0
+        indexcol = 0
+        cov=np.zeros((len(binning['Cdd'])*len(ell),len(binning['Cdd'])*len(ell)))
+
+        for key_row in binning['Cdd']:
+            for key_col in binning['Cdd']:
+
+                i = key_row[0]
+                j = key_row[1]
+                m = key_col[0]
+                n = key_col[1]
+
+                obs_c_ell_im = switch_keys(str(i), str(m), obs_c_ell)
+                obs_c_ell_jn = switch_keys(str(j), str(n), obs_c_ell)
+                obs_c_ell_in = switch_keys(str(i), str(n), obs_c_ell)
+                obs_c_ell_jm = switch_keys(str(j), str(m), obs_c_ell)
+
+                prefactor = 1./((2*ell+1)*delta_ell*fsky)
+
+                mini_cov = np.zeros((len(ell),len(ell)))
+                for a in range(len(ell)):
+                    for b in range(len(ell)):
+                        if a==b:
+                            mini_cov[a][b] = obs_c_ell_im[a]*obs_c_ell_jn[b] + obs_c_ell_in[a]*obs_c_ell_jm[b]
+
+                cov[indexrow*len(ell):indexrow*len(ell)+len(ell),indexcol*len(ell):indexcol*len(ell)+len(ell)] = prefactor*mini_cov
+                print(prefactor)
+                print(mini_cov)
+
+                indexcol += 1
+                if indexcol == 10:
+                    indexrow += 1
+                    indexcol = 0
+        return cov
+
     def save_outputs_firecrown(self,cov,data_vector,ells,nz,binning):
         #Saving as a CSV file for now because FireCrown can import this but this
         # may eventually be saved in SACC.
@@ -218,16 +375,17 @@ class TXFourierGaussianCovariance(PipelineStage):
         vals = [cov[iis[x]][jjs[x]] for x in range(len(iis))]
         cov_dic = {'i':iis,'j':jjs,'cov':vals}
         cov_df = pd.DataFrame(cov_dic)
-        cov_output.save_file(cov_output_name,cov_df)
+        #cov_output.save_file(cov_output_name,cov_df)
+        np.save('./outputs/cov_test.npy',cov)
 
         data_output = CSVFile()
-        data_out_name = 'data_vec.csv'
+        data_out_name = 'outputs/data_vec.csv'
         data_output_dic = {'ell':ells, 'measured_statistic':data_vector}
         data_output_df = pd.DataFrame(data_output_dic)
-        data_output_df.save_file(data_output_name,data_output_df)
+        #data_output_df.save_file(data_output_name,data_output_df)
 
         nz_output = CSVFile()
-        nz_out_name = 'nz_vec.csv'
+        nz_out_name = '/outputs/nz_vec.csv'
         zs = []
         dndzs = []
         #Change this to reference the binning
@@ -239,4 +397,4 @@ class TXFourierGaussianCovariance(PipelineStage):
             dndzs+= nz.get('bin_'+bin)
         nz_output_dic = {'z':nz['z'], 'dndz':dndzs}
         nz_output_df = pd.DataFrame(nz_output_dic)
-        nz_output_df.save_file(nz_output_name,nz_output_df)
+        #nz_output_df.save_file(nz_output_name,nz_output_df)
