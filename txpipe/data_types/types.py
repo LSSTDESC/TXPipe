a7fd572211010f55d9fec4bb6fbdafa2da8da953
Joe Zuntz <joezuntz@googlemail.com>
2019-03-14 15:21:01 +0000

    add NofZ file to TXPipe data types (from descformats)

--- txpipe/data_types/types.py
+++ txpipe/data_types/types.py
@@ -153,3 +153,47 @@ class PhotozPDFFile(HDFFile):
 class SACCFile(HDFFile):
     suffix = 'sacc'
 
+
+
+class NOfZFile(HDFFile):
+    # Must have at least one bin in
+    required_datasets = ['n_of_z/lens/z', 'n_of_z/source/bin_0']
+
+    def validate(self):
+        super().validate()
+
+        for kind in ('lens', 'source'):
+            nbin = self.get_nbin(kind)
+            for b in range(nbin):
+                col_name = 'bin_{}'.format(b)
+                if not col_name in self.file[f'n_of_z/{kind}']:
+                    raise FileValidationError(f"Expected to find {nbin} bins in NOfZFile but was missing at least {col_name}")
+
+    def get_nbin(self, kind):
+        return self.file['n_of_z'][kind].attrs['nbin']
+
+    def get_n_of_z(self, kind, bin_index):
+        group = self.file['n_of_z'][kind]
+        z = group['z'][:]
+        nz = group[f'bin_{bin_index}'][:]
+        return (z, nz)
+
+    def get_n_of_z_spline(self, bin_index, kind='cubic', **kwargs):
+        import scipy.interpolate
+        z, nz = self.get_n_of_z(bin_index)
+        spline = scipy.interpolate.interp1d(z, nz, kind=kind, **kwargs)
+        return spline
+
+    def save_plot(self, filename, **fig_kw):
+        import matplotlib.pyplot as plt
+        plt.figure(**fig_kw)
+        self.plot()
+        plt.legend()
+        plt.savefig(filename)
+        plt.close()
+
+    def plot(self):
+        import matplotlib.pyplot as plt
+        for b in range(self.get_nbin()):
+            z, nz = self.get_n_of_z(b)
+            plt.plot(z, nz, label=f'Bin {b}')
