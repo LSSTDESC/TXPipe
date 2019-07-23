c5ce01ff197356e13c734215cd83bdf50dc68ef2
Joe Zuntz <joezuntz@googlemail.com>
2019-07-23 13:37:35 +0100

    rename file

--- txpipe/__init__.py
+++ txpipe/__init__.py
@@ -17,5 +17,5 @@ from .photoz_mlz import PZPDFMLZ
 from .covariance import TXFourierGaussianCovariance
 from .metacal_gcr_input import TXMetacalGCRInput
 from .diagnostics import TXDiagnosticPlots
-from .rowe_stats import TXRoweStatistics
+from .psf_plots import TXRoweStatistics
 from .exposure_info import TXExposureInfo
