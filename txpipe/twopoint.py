f0dcfb90cdad05e145f0def083f20203d019aee5
Anze Slosar <anze@cori01.nersc.gov>
2020-02-18 05:23:38 -0800

    working version

--- txpipe/twopoint.py
+++ txpipe/twopoint.py
@@ -46,14 +46,15 @@ class TXTwoPoint(PipelineStage):
         'do_shear_shear': True,
         'do_shear_pos': True,
         'do_pos_pos': True,
-        'blind': False,
+        'blind': True,
         'blind_seed': 1972,  ## seed uniquely specifies the shift in parameters
         'blind_Omega_b': [0.0485, 0.001], ## fiducial_model_value, shift_sigma
         'blind_Omega_c': [0.2545, 0.01],
         'blind_w0': [-1.0, 0.1],
         'blind_h': [0.682, 0.02],
         'blind_sigma8': [0.801, 0.01],
-        'blind_n_s': [0.971, 0.03]
+        'blind_n_s': [0.971, 0.03],
+        'blind_b0': 0.95
       }
 
     def run(self):
@@ -243,6 +244,14 @@ class TXTwoPoint(PipelineStage):
             'sigma8': self.config['blind_sigma8'][0],
             'n_s': self.config['blind_n_s'][0],
         }
+        ## now get biases
+        bz={}
+        fidCosmo=ccl.Cosmology(**fid_params)
+        for key,tracer in sacc.tracers.items():
+            if 'lens' in key:
+                zeff = (tracer.z*tracer.nz).sum()/tracer.nz.sum()
+                bz[key] = self.config['blind_b0']/ccl.growth_factor(fidCosmo,1/(1+zeff)) 
+        
         offset_params = copy.copy(fid_params)
         for par in fid_params.keys():
             offset_params [par] += self.config['blind_'+par][1]*np.random.normal(0.,1.)
@@ -253,7 +262,10 @@ class TXTwoPoint(PipelineStage):
              wa: 0.0
 
              one: 1
-           
+"""
+        for k,v in bz.items():
+            fc_config+="             bias_{lens}: {bias}\n".format(lens=k,bias=v)
+        fc_config += """
            two_point:
              module: firecrown.ccl.two_point
            
@@ -280,33 +292,41 @@ class TXTwoPoint(PipelineStage):
                 fc_config+="""
                                   {lens}:
                                      kind: NumberCountsSource
-                                     bias: one
+                                     bias: bias_{lens}
                                      sacc_tracer: {lens}\n""".format(lens=key)
                 lenslist.append(key)
         fc_config+="             statistics: \n"
         ##
-        ## Something dummy to start with
-        fc_config+="""
-                                 cl_src0_src0:
-                                   sources: ['source_0', 'source_0']
-                                   sacc_data_type: galaxy_shear_xi_plus
-                   """
-                
+        ## list(dict.fromkeys()) gives unique elements while preserving order
+        types=list(dict.fromkeys([(p.data_type, p.tracers,
+                                   "{dtype}_{tracer1}_{tracer2}".format(dtype=p.data_type,
+                                            tracer1=p.tracers[0],tracer2=p.tracers[1]))
+                                   for p in sacc.data]))
+        for dtype,(tracer1,tracer2),fcname in types:
+            fc_config+="""
+                                 {fcname}:
+                                   sources: ['{tracer1}', '{tracer2}']
+                                   sacc_data_type: {dtype}
+                   """.format(fcname=fcname, dtype=dtype,tracer1=tracer1,tracer2=tracer2)
         ## now try to get predictions
-        config, data = firecrown.parse(io.StringIO(fc_config),
-               settings={'two_point':{'sacc_data':sacc},'parameters':fid_params})
-        fidcosmo = firecrown.get_ccl_cosmology(config['parameters'])
-        _, fidstat = firecrown.compute_loglike(cosmo=fidcosmo, data=data)
+        pred={}
+        for name,pars in [('fid',fid_params),('ofs',offset_params)]:
+            print ("Calling firecrown : %s"%(name))
 
-        config, data = firecrown.parse(io.StringIO(fc_config),
-               settings={'two_point':{'sacc_data':sacc},'parameters':offset_params})
-        offsetcosmo = firecrown.get_ccl_cosmology(config['parameters'])
-        _, offsetstat = firecrown.compute_loglike(cosmo=offsetcosmo, data=data)
+            config, data = firecrown.parse(io.StringIO(fc_config),
+               settings={'two_point':{'sacc_data':sacc},'parameters':pars})
+            cosmo = firecrown.get_ccl_cosmology(config['parameters'])
+            firecrown.compute_loglike(cosmo=cosmo, data=data)
+            pred[name] = np.hstack([data['two_point']['data']['statistics'][n].predicted_statistic_ for
+                                  _,_,n in types])
+            ## if there is some sanity this should work
+            assert(len(pred[name])==len(sacc))
 
-        
-        print ("Not quite done...")
-            
-
+        diffvec = pred['ofs']-pred['fid']
+        ## now add offsets
+        for p, delta in zip(sacc.data,diffvec):
+            p.value+=delta
+        print ("Blinding done.")
             
         return  sacc
     
