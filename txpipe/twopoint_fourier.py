e75867db90f1257804abb200109a5945ab58cc34
Joe Zuntz <joezuntz@googlemail.com>
2020-02-27 18:03:43 +0000

    Fixes to twopoint_fourier for DC2

--- txpipe/twopoint_fourier.py
+++ txpipe/twopoint_fourier.py
@@ -3,6 +3,7 @@ from .data_types import MetacalCatalog, TomographyCatalog, RandomsCatalog, YamlF
 import numpy as np
 import collections
 from .utils import choose_pixelization, HealpixScheme, GnomonicPixelScheme, ParallelStatsCalculator
+from .utils.theory import theory_3x2pt
 import sys
 import warnings
 
@@ -41,7 +42,7 @@ class TXTwoPointFourier(PipelineStage):
         ('photoz_stack', HDFFile),  # Photoz stack
         ('diagnostic_maps', DiagnosticMaps),
         ('fiducial_cosmology', YamlFile),  # For the cosmological parameters
-        ('tomography_catalog', TomographyCatalog),  # For density info
+        ('tracer_metdata', TomographyCatalog),  # For density info
     ]
     outputs = [
         ('twopoint_data_fourier', SACCFile)
@@ -59,6 +60,7 @@ class TXTwoPointFourier(PipelineStage):
         import pymaster
         import healpy
         import sacc
+        import pyccl
         config = self.config
 
         if self.comm:
@@ -80,19 +82,22 @@ class TXTwoPointFourier(PipelineStage):
 
         # Load in the per-bin auto-correlation noise levels and 
         # mean response values
-        noise, mean_R = self.load_tomographic_quantities(nbin_source, nbin_lens, f_sky)
+        tomo_info = self.load_tomographic_quantities(nbin_source, nbin_lens, f_sky)
 
-        # Load the n(z) values, which are both saved in the output
-        # file alongside the spectra, and used to calcualate the
-        # fiducial theory C_ell
-        tracers = self.load_tracers(nbin_source, nbin_lens)
 
         # Binning scheme, currently chosen from the geometry.
         # TODO: set ell binning from config
         ell_bins = self.choose_ell_bins(pixel_scheme, f_sky)
 
-        # This is needed in the deprojection calculation
-        theory_cl = self.fiducial_theory(tracers, ell_bins, nbin_source, nbin_lens)
+        # Load the n(z) values, which are both saved in the output
+        # file alongside the spectra, and then used to calcualate the
+        # fiducial theory C_ell, which is used in the deprojection calculation
+        tracers = self.load_tracers(nbin_source, nbin_lens)
+        theory_cl = theory_3x2pt(
+            self.get_input('fiducial_cosmology'),
+            tracers,
+            nbin_source, nbin_lens,
+            fourier=True)
 
         # If we are rank zero print out some info
         if self.rank==0:
@@ -119,7 +124,7 @@ class TXTwoPointFourier(PipelineStage):
         # as it's not dynamic, just a round-robin assignment.
         for i, j, k in self.split_tasks_by_rank(calcs):
             self.compute_power_spectra(
-                pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins, noise, theory_cl)
+                pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins, tomo_info, theory_cl)
 
         if self.rank==0:
             print(f"Collecting results together")
@@ -130,7 +135,6 @@ class TXTwoPointFourier(PipelineStage):
         if self.rank == 0:
             self.save_power_spectra(tracers, nbin_source, nbin_lens)
             print("Saved power spectra")
-
 
 
 
@@ -165,12 +169,13 @@ class TXTwoPointFourier(PipelineStage):
         if self.rank == 0:
             print(f"Unmasked area = {area}, fsky = {f_sky}")
 
-
         # Load all the maps in.
         # TODO: make it possible to just do a subset of these
         ngal_maps = [map_file.read_map(f'ngal_{b}') for b in range(nbin_lens)]
         g1_maps = [map_file.read_map(f'g1_{b}') for b in range(nbin_source)]
         g2_maps = [map_file.read_map(f'g2_{b}') for b in range(nbin_source)]
+        lensing_weights = [map_file.read_map(f'lensing_weight_{b}') for b in range(nbin_source)]
+        depth_map = map_file.read_map(f'depth')
 
         # Mask any pixels which have the healpix bad value
         for m in g1_maps + g2_maps + ngal_maps:
@@ -206,62 +211,61 @@ class TXTwoPointFourier(PipelineStage):
         # and then use that to convert to overdensity
         d_maps = [(ng/mu)-1 for (ng,mu) in zip(ngal_maps, n_means)]
 
-        d_fields = []
-        wl_fields = []
+
+        density_fields = []
+        lensing_fields = []
 
         if pixel_scheme.name == 'gnomonic':
             lx = np.radians(pixel_scheme.size_x)
             ly = np.radians(pixel_scheme.size_y)
 
-            # Apodize the mask
-            if apod_size > 0:
-                if self.rank==0:
-                    print(f"Apodizing mask with size {apod_size} deg and method {apod_type}")
-                mask = nmt.mask_apodization_flat(mask, lx, ly, apod_size, apotype=apod_type)
-            elif self.rank==0:
-                print("Not apodizing mask.")
+        # For the lensing mask we optionall apodize the binary mask.
+        # TODO: include the masked object fraction in here.
+        # TODO: ask about including the depth map in here
+        if apod_size > 0:
+            if self.rank==0:
+                print(f"Apodizing mask with size {apod_size} deg and method {apod_type}")
 
-            for i,d in enumerate(d_maps):
-                # Density for gnomonic maps
-                if self.rank == 0:
-                    print(f"Generating gnomonic density field {i}")
-                field = nmt.NmtFieldFlat(lx, ly, mask, [d], templates=syst_nc) 
-                d_fields.append(field)
-
-            for i,(g1,g2) in enumerate(zip(g1_maps, g2_maps)):
-                # Density for gnomonic maps
-                if self.rank == 0:
-                    print(f"Generating gnomonic lensing field {i}")
-                field = nmt.NmtFieldFlat(lx, ly, mask, [g1,g2], templates=syst_wl) 
-                wl_fields.append(field)
-            
-
-        elif pixel_scheme.name == 'healpix':
-            # Apodize the mask
-            if apod_size > 0:
-                if self.rank==0:
-                    print(f"Apodizing mask with size {apod_size} deg and method {apod_type}")
-                mask = nmt.mask_apodization(mask, apod_size, apotype=apod_type)
-            elif self.rank==0:
-                print("Not apodizing mask.")
-
-
-            for i,d in enumerate(d_maps):
-                # Density for gnomonic maps
-                print(f"Generating healpix density field {i}")
-                field = nmt.NmtField(mask, [d], templates=syst_nc)
-                d_fields.append(field)
-
-            for i,(g1,g2) in enumerate(zip(g1_maps, g2_maps)):
-                # Density for gnomonic maps
-                print(f"Generating healpix lensing field {i}")
-                field = nmt.NmtField(mask, [g1, g2], templates=syst_wl) 
-                wl_fields.append(field)
-
+            if pixel_scheme.name == 'gnomonic':
+                    lens_mask = nmt.mask_apodization_flat(mask, lx, ly, apod_size, apotype=apod_type)
+            elif pixel_scheme.name == 'healpix':
+                lens_mask = nmt.mask_apodization(mask, apod_size, apotype=apod_type)
+            else:
+                raise ValueError(f"Pixelization scheme {pixel_scheme.name} not supported by NaMaster")
         else:
-            raise ValueError(f"Pixelization scheme {pixel_scheme.name} not supported by NaMaster")
+            lens_mask = mask
 
-        return pixel_scheme, d_fields, wl_fields, nbin_source, nbin_lens, f_sky
+
+        # we do apodize the density masks, but not the WL ones
+        for i,d in enumerate(d_maps):
+            if self.rank == 0:
+                print(f"Generating density field {i}")
+            if pixel_scheme.name == 'gnomonic':
+                field = nmt.NmtFieldFlat(lx, ly, lens_mask, [d], templates=syst_nc) 
+            elif pixel_scheme.name == 'healpix':
+                field = nmt.NmtField(lensing_weights[i], [d], templates=syst_nc)
+            else:
+                raise ValueError(f"Pixelization scheme {pixel_scheme.name} not supported by NaMaster")
+            density_fields.append(field)
+
+
+
+        for i,(g1,g2) in enumerate(zip(g1_maps, g2_maps)):
+            # Density for gnomonic maps
+            if self.rank == 0:
+                print(f"Generating lensing field {i}")
+            if pixel_scheme.name == 'gnomonic':
+                field = nmt.NmtFieldFlat(lx, ly, wmask, [g1,g2], templates=syst_wl) 
+            elif pixel_scheme.name == 'healpix':
+                field = nmt.NmtField(mask, [g1, g2], templates=syst_wl) 
+            else:
+                raise ValueError(f"Pixelization scheme {pixel_scheme.name} not supported by NaMaster")
+
+            lensing_fields.append(field)
+        
+
+
+        return pixel_scheme, density_fields, lensing_fields, nbin_source, nbin_lens, f_sky
 
 
     def collect_results(self):
@@ -356,7 +360,7 @@ class TXTwoPointFourier(PipelineStage):
 
         return calcs
 
-    def compute_power_spectra(self, pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins, noise, cl_theory):
+    def compute_power_spectra(self, pixel_scheme, i, j, k, f_wl, f_d, w00, w02, w22, ell_bins, tomo_info, cl_theory):
         # Compute power spectra
         # TODO: now all possible auto- and cross-correlation are computed.
         #      This should be tunable.
@@ -375,106 +379,111 @@ class TXTwoPointFourier(PipelineStage):
         print(f"Process {self.rank} calcluating {type_name} spectrum for bin pair {i},{i}")
         sys.stdout.flush()
 
-        # We need the theory spectrum for this pair
-        theory = cl_theory[(i,j,k)]
 
         # The binning information - effective (mid) ell values and
         # the window information
         ls = ell_bins.get_effective_ells()
         win = [ell_bins.get_window(b) for b,l  in enumerate(ls)]
 
+        # We need the theory spectrum for this pair
+        #TODO: when we have templates to deproject, use this.
+        theory = cl_theory[(i,j,k)]
+        ell_guess = cl_theory['ell']
+        cl_guess = None
+
         if k == SHEAR_SHEAR:
             workspace = w22
             field_i = f_wl[i]
             field_j = f_wl[j]
-            cl_guess = [theory, np.zeros_like(theory), np.zeros_like(theory), np.zeros_like(theory)]
             results_to_use = [(0, CEE), (3, CBB)]
 
         elif k == POS_POS:
             workspace = w00
             field_i = f_d[i]
             field_j = f_d[j]
-            cl_guess = [theory]
             results_to_use = [(0, Cdd)]
 
         elif k == SHEAR_POS:
             workspace = w02
             field_i = f_wl[i]
             field_j = f_d[j]
-            cl_guess = [theory, np.zeros_like(theory)]
             results_to_use = [(0, CdE), (1, CdB)]
 
-        cl_noise = self.compute_noise(i,j,k,ell_bins,workspace,noise)
+        cl_noise = self.compute_noise(i,j,k,ell_bins,workspace,tomo_info)
 
         if pixel_scheme.name == 'healpix':
             c = pymaster.compute_full_master(field_i, field_j, ell_bins,
                 cl_noise=cl_noise, cl_guess=cl_guess, workspace=workspace)
         elif pixel_scheme.name == 'gnomonic':
             c = pymaster.compute_full_master_flat(field_i, field_j, ell_bins,
-                cl_noise=cl_noise, cl_guess=cl_guess, ells_guess=cl_theory['ell'],
+                cl_noise=cl_noise, cl_guess=cl_guess, ells_guess=ell_guess,
                 workspace=workspace)
 
         for index, name in results_to_use:
             self.results.append(Measurement(name, ls, c[index], win, i, j))
 
 
-    def compute_noise(self, i, j, k, ell_bins, w, noise):
-        # No noise contribution from cross-correlations
+    def compute_noise(self, i, j, k, ell_bins, w, tomo_info):
+        # No noise contribution in cross-correlations
         if (i!=j) or (k==SHEAR_POS):
             return None
-        # print("Setting noise to zero")
-        # return None
-        # We loaded in sigma_e and the densities
-        # earlier on and put them in the noise dictionary
-        noise_level = noise[(i,k)]
 
+        # We loaded in sigma_e and the densities
+        # earlier on, and put them in the tomo_info dictionary
+        if k==SHEAR_SHEAR:
+            noise_level = tomo_info['sigma_e'][i]**2 / tomo_info['n_eff_steradian'][i]
+        else:
+            noise_level = 1.0 / tomo_info['n_lens_steradian'][i]
+
+        # Number of ell values in the uncoupled C_ell (banded in flat case)
         if ell_bins.is_flat():
             n = ell_bins.get_n_bands()
         else:
             n = w.wsp.lmax + 1
 
-        # ell-by-ell noise level of the right size
-        N1 = np.ones(n) * noise[(i,k)]
+        # Uncoupled noise C_ell value
+        N1 = np.ones(n) * noise_level
 
-        # # Need the same noise for EE, BB, EB, BE
-        # # or PE, PB
         if k==SHEAR_SHEAR:
             N2 = [N1, N1, N1, N1]
-        elif k==SHEAR_POS:
-            N2 = [N1, N1]
         else:
             N2 = [N1]
 
+        # Couple to take coupled C_ell value
         if ell_bins.is_flat():
             ell = ell_bins.get_effective_ells()
-            N3 = w.couple_cell(ell, N2)
+            cl_noise = w.couple_cell(N2)
         else:
-            N3 = w.couple_cell(N2)
-        return N3
+            cl_noise = w.couple_cell(N2)
+
+        return cl_noise
 
 
     def load_tomographic_quantities(self, nbin_source, nbin_lens, f_sky):
-        tomo = self.open_input('tomography_catalog')
-        sigma_e = tomo['tomography/sigma_e'][:]
-        mean_R = tomo['multiplicative_bias/R_gamma_mean'][:]
-        N_eff = tomo['tomography/N_eff'][:]
-        lens_counts = tomo['tomography/lens_counts'][:]
-        tomo.close()
+        metadata = self.open_input('tracer_metdata')
+        sigma_e = metadata['tracers/sigma_e'][:]
+        mean_R = metadata['tracers/R_gamma_mean'][:]
+        N_eff = metadata['tracers/N_eff'][:]
+        lens_counts = metadata['tracers/lens_counts'][:]
+        metadata.close()
 
         area = 4*np.pi*f_sky
-
         n_eff = N_eff / area
         n_lens = lens_counts / area
 
-        noise = {}
-        for i in range(nbin_source):
-            noise[(i,SHEAR_SHEAR)] = f_sky*sigma_e[i]**2 / n_eff[i]
-        for i in range(nbin_lens):
-            noise[(i,POS_POS)] = f_sky / n_lens[i]
+        tomo_info = {
+            "area_steradians": area,
+            "n_eff_steradian": n_eff,
+            "n_lens_steradian": n_lens,
+            "sigma_e": sigma_e,
+            "f_sky": f_sky,
+            "mean_R": mean_R,
+        }
+
 
         warnings.warn("Using unweighted lens samples here")
 
-        return noise, mean_R
+        return tomo_info
 
 
 
@@ -500,70 +509,7 @@ class TXTwoPointFourier(PipelineStage):
 
         return tracers
 
-    def fiducial_theory(self, tracers, ell_bins, nbin_source, nbin_lens):
-        import pyccl as ccl
 
-        filename = self.get_input('fiducial_cosmology')
-        cosmo = ccl.Cosmology.read_yaml(filename)
-
-        # We will need the theory C_ell in a continuum up until
-        # the full ell_max, because we will need a weighted sum
-        # over the values
-        ell_max = np.max(ell_bins.ell_max)
-        #f_d[0].fl.lmax
-        ell = np.arange(ell_max+1, dtype=int)
-
-        # Convert from SACC tracers (which just store N(z))
-        # to CCL tracers (which also have cosmology info in them).
-        CTracers = {}
-
-        # Lensing tracers - need to think a little more about
-        # the fiducial intrinsic alignment here
-        for i in range(nbin_source):
-            x = tracers[f'source_{i}']
-            tag = ('S', i)
-            CTracers[tag] = ccl.WeakLensingTracer(cosmo, dndz=(x.z, x.nz))
-        # Position tracers - even more important to think about fiducial biases
-        # here - these will be very very wrong otherwise!
-        # Important enough that I'll put in a warning.
-        warnings.warn("Not using galaxy bias in fiducial theory density spectra")
-
-        for i in range(nbin_lens):
-            x = tracers[f'lens_{i}']
-            tag = ('P', i) 
-            b = np.ones_like(x.z)
-            CTracers[tag] = ccl.NumberCountsTracer(cosmo, dndz=(x.z, x.nz),
-                                        has_rsd=False, bias=(x.z,b))
-
-        # Use CCL to actually calculate the C_ell values for the different cases
-        theory_cl = {}
-        theory_cl['ell'] = ell
-        k = SHEAR_SHEAR
-        for i in range(nbin_source):
-            for j in range(i+1):
-                Ti = CTracers[('S',i)]
-                Tj = CTracers[('S',j)]
-                # The full theory C_ell over the range 0..ellmax
-                theory_cl [(i,j,k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
-                
-
-        # The same for the galaxy galaxy-lensing cross-correlation
-        k = SHEAR_POS
-        for i in range(nbin_source):
-            for j in range(nbin_lens):
-                Ti = CTracers[('S',i)]
-                Tj = CTracers[('P',j)]
-                theory_cl [(i,j,k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
-
-        # And finally for the density correlations
-        k = POS_POS
-        for i in range(nbin_lens):
-            for j in range(i+1):
-                Ti = CTracers[('P',i)]
-                Tj = CTracers[('P',j)]
-                theory_cl [(i,j,k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
-
-        return theory_cl
 
 
     def save_power_spectra(self, tracers, nbin_source, nbin_lens):
