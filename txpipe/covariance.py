6e6137a14bc2c91ec11472e67411cd4108373fdc
Joe Zuntz <joezuntz@googlemail.com>
2020-03-31 10:45:03 +0100

    round printout

--- txpipe/covariance.py
+++ txpipe/covariance.py
@@ -134,7 +134,7 @@ class TXFourierGaussianCovariance(PipelineStage):
         print(f"n_eff:  {n_eff} / steradian")
         print(f"     =  {np.around(n_eff_arcmin,2)} / sq arcmin")
         print(f"lens density: {n_lens} / steradian")
-        print(f"            = {n_lens} / arcmiin")
+        print(f"            = {np.around(n_lens_arcmin,2)} / arcmin")
 
         # Pass all this back as a dictionary
         meta = {
