4ae35b1875fd35f4c6fcdfa2c43ba937626466c1
Joe Zuntz <joezuntz@googlemail.com>
2020-05-29 14:04:29 +0100

    make cutter better

--- bin/cut_out_example.py
+++ bin/cut_out_example.py
@@ -1,52 +1,43 @@
 import h5py
 import numpy as np
-#generate_subset.py
+import argparse
+parser = argparse.ArgumentParser(description='Cut out a subset')
 
 ra_min = 60
 ra_max = 61
 dec_min = -31
 dec_max = -30
 
-photo_in = h5py.File('photometry_catalog.hdf5', 'r')
-photo_out = h5py.File('example_photometry_catalog.hdf5', 'w')
 
-thin = 5
+def trim_file(input_file, output_file, group_name, thin):
+    file_in = h5py.File(input_file, 'r')
+    file_out = h5py.File(output_file, 'w')
 
+    group_in = photo_in[group_name]
+    group_out = photo_out.create_group(group_name)
 
-group_in = photo_in['photometry']
-group_out = photo_out.create_group('photometry')
+    file_in.copy('provenance', file_out)
 
-photo_in.copy('provenance', photo_out)
+    ra = group_in['ra'][:]
+    print("read RA", ra.size)
+    dec = group_in['dec'][:]
+    print("read dec")
+    w  = ra > ra_min
+    w &= ra < ra_max
+    w &= dec > dec_min
+    w &= dec < dec_max
+    del ra, dec
+    w[np.arange(w.size) % thin > 0] = False
 
-ra = group_in['ra'][:]
-print("read RA", ra.size)
-dec = group_in['dec'][:]
-print("read dec")
-w  = ra > ra_min
-w &= ra < ra_max
-w &= dec > dec_min
-w &= dec < dec_max
-del ra, dec
-w[np.arange(w.size) % thin > 0] = False
+    print('Output count:', w.sum())
 
-
-print('Output count:', w.sum())
-
-for col in group_in.keys():
-    print("Copying", col)
-    d = group_in[col][w]
-    group_out.create_dataset(col, data=d)
+    for col in group_in.keys():
+        print("Copying", col)
+        d = group_in[col][w]
+        group_out.create_dataset(col, data=d)
 
 
-shear_in = h5py.File('shear_catalog.hdf5', 'r')
-shear_out = h5py.File('example_shear_catalog.hdf5', 'w')
-
-shear_in.copy('provenance', shear_out)
-
-group_in = shear_in['metacal']
-group_out = shear_out.create_group('metacal')
-
-for col in group_in.keys():
-    print("Copying", col)
-    d = group_in[col][w]
-    group_out.create_dataset(col, data=d)
+if __name__ == '__main__':
+    trim_file('photometry_catalog.hdf5', 'example_photometry_catalog.hdf5', 'photometry', 5)
+    trim_file('shear_catalog.hdf5', 'example_shear_catalog.hdf5', 'photometry', 5)
+    trim_file('star_catalog.hdf5', 'example_star_catalog.hdf5', 'photometry', 5)
