c449018c16465fc9a2cc58a15190d0effe66963a
Joe Zuntz <joezuntz@googlemail.com>
2020-06-17 14:57:55 +0100

    add unique list and rename

---
+++ txpipe/utils/misc.py
@@ -0,0 +1,33 @@
+import hashlib
+
+
+
+def array_hash(x):
+    b = x.tobytes()
+    # We do not need a cryptographic hash here
+    return int(hashlib.md5(b).hexdigest(), 16)
+
+
+
+def unique_list(seq):
+    """
+    Find the unique elements in a list or other sequence
+    while maintaining the order. (i.e., remove any duplicated
+    elements but otherwise leave it the same)
+
+    Method from:
+    https://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-whilst-preserving-order
+
+    Parameters
+    ----------
+    seq: list or sequence
+        Any input object that can be iterated
+
+    Returns
+    -------
+    L: list
+        a new list of the unique objects
+    """
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
