726ee2ae9ff630e8c838e8eaef16041d53e602c7
joezuntz <joezuntz@googlemail.com>
2020-06-23 03:29:32 -0700

    fix mask

--- txpipe/masks.py
+++ txpipe/masks.py
@@ -15,23 +15,26 @@ class TXSimpleMask(PipelineStage):
     }        
 
     def run(self):
+        import healpy
+        
         with self.open_input('aux_maps', wrapper=True) as f:
             metadata = dict(f.file['maps'].attrs)
             bright_obj = f.read_map('bright_objects/count')
             depth = f.read_map('depth/depth')
 
-
+        hit = depth != healpy.UNSEEN
         masks = [
             ('depth', depth > self.config['depth_cut']) ,
             ('bright_obj', bright_obj < self.config['bright_object_max'])
         ]
 
         for name, m in masks:
-            frac = 1 - m.sum() / hit.sum()
-            print(f"Mask '{name}' removes fraction {frac:.2f} of hit pixels")
+            frac = 1 - (m & hit).sum() / hit.sum()
+            print(f"Mask '{name}' removes fraction {frac:.3f} of hit pixels")
 
         # Overall mask
-        mask = np.logical_and([mask for _, mask in masks])
+        mask = np.logical_and.reduce([mask for _, mask in masks])
+
 
         f_sky = (mask.sum() * 1.0) / mask.size
         area = f_sky * 41252.96125
