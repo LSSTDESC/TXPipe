fixes to masking and calibration in maps

txpipe/maps.py
@@ -218,6 +218,7 @@ class TXSourceMaps(TXBaseMaps):
         mapper.add_data(data)
 
     def calibrate_map_metacal(self, g1, g2, var_g1, var_g2, R):
+
         g1, g2 = apply_metacal_response(R, 0, g1, g2)
 
         std_g1 = np.sqrt(var_g1)
@@ -250,6 +251,9 @@ class TXSourceMaps(TXBaseMaps):
         return g1, g2, var_g1, var_g2
 
     def calibrate_maps(self, g1, g2, var_g1, var_g2, cal):
+        import healpy
+
+        # We will return lists of calibrated maps
         g1_out = []
         g2_out = []
         var_g1_out = []
@@ -258,6 +262,14 @@ class TXSourceMaps(TXBaseMaps):
         n = len(g1)
 
         for i in range(n):
+            # we want to avoid accidentally calibrating any pixels
+            # that should be masked.
+            mask = (
+                  (g1[i] == healpy.UNSEEN)
+                | (g2[i] == healpy.UNSEEN)
+                | (var_g1[i] == healpy.UNSEEN)
+                | (var_g2[i] == healpy.UNSEEN)
+            )
             if self.config['shear_catalog_type'] == 'metacal':
                 R = cal[0]
                 out = self.calibrate_map_metacal(g1[i], g2[i], var_g1[i], var_g2[i], R[i])
@@ -266,6 +278,14 @@ class TXSourceMaps(TXBaseMaps):
                 out = self.calibrate_map_lensfit(g1[i], g2[i], var_g1[i], var_g2[i], R[i], K[i], c[i])
             else:
                 raise ValueError("Unknown calibration")
+            # re-apply the masking, just to make sure
+            g1_i, g2_i, v1_i, v2_i = out
+            g1[mask] = healpy.UNSEEN
+            g2[mask] = healpy.UNSEEN
+            var_g1[mask] = healpy.UNSEEN
+            var_g2[mask] = healpy.UNSEEN
+
+            # append our results for this tomographic bin
             g1_out.append(out[0])
             g2_out.append(out[1])
             var_g1_out.append(out[2])
@@ -295,8 +315,8 @@ class TXSourceMaps(TXBaseMaps):
             maps["source_maps", f"g1_{b}"] = (pix, g1[b])
             maps["source_maps", f"g2_{b}"] = (pix, g2[b])
             maps["source_maps", f"var_g1_{b}"] = (pix, var_g1[b])
-            maps["source_maps", f"var_g1_{b}"] = (pix, var_g1[b])
-            maps["source_maps", f"lensing_weight_{b}"] = (pix, var_g1[b])
+            maps["source_maps", f"var_g2_{b}"] = (pix, var_g2[b])
+            maps["source_maps", f"lensing_weight_{b}"] = (pix, weights_g[b])
 
         return maps
 
@@ -524,8 +544,8 @@ class TXMainMaps(TXSourceMaps, TXLensMaps):
             maps["source_maps", f"g1_{b}"] = (pix, g1[b])
             maps["source_maps", f"g2_{b}"] = (pix, g2[b])
             maps["source_maps", f"var_g1_{b}"] = (pix, var_g1[b])
-            maps["source_maps", f"var_g1_{b}"] = (pix, var_g1[b])
-            maps["source_maps", f"lensing_weight_{b}"] = (pix, var_g1[b])
+            maps["source_maps", f"var_g2_{b}"] = (pix, var_g2[b])
+            maps["source_maps", f"lensing_weight_{b}"] = (pix, weights_g[b])
 
         return maps
 