fix initial mean shear calculation

txpipe/source_selector.py
@@ -1,7 +1,7 @@
 from .base_stage import PipelineStage
 from .data_types import ShearCatalog, YamlFile, PhotozPDFFile, TomographyCatalog, HDFFile, TextFile
 from .utils import SourceNumberDensityStats
-from .utils.calibration_tools import read_shear_catalog_type
+from .utils.calibration_tools import read_shear_catalog_type, apply_metacal_response
 from .utils.calibration_tools import metacal_variants, band_variants, ParallelCalibratorMetacal, ParallelCalibratorNonMetacal
 import numpy as np
 import warnings
@@ -446,21 +446,33 @@ class TXSourceSelector(PipelineStage):
         mean_e1 = np.zeros(nbin_source)
         mean_e2 = np.zeros(nbin_source)
 
-        # this needs fixing
         means, sigma_e = number_density_stats.collect()
 
+
         for i, cal in enumerate(calibrators):
+            mu1 = np.array([means[i, 0]])
+            mu2 = np.array([means[i, 1]])
             if self.config['shear_catalog_type']=='metacal':
+                # Collect the total calibration factor
                 R[i], S[i], N[i] = cal.collect(self.comm)
-                sigma_e[i] /= 0.5*(R[i,0,0] + R[i,1,1])
-                mean_e1[i] = means[0] / R[i,0,0]
-                mean_e2[i] = means[1] / R[i,1,1]
-            else:
+
+                # Apply it to the means
+                mean_e1[i], mean_e2[i] = apply_metacal_response(
+                    R[i], S[i], g1=mu1, g2=mu2)
+
+                # TODO: use full R matrix here?
+                sigma_e[i] /= 0.5 * (R[i,0,0] + R[i,1,1])
+            elif self.config['shear_catalog_type']=='lensfit':
+                # lensfit case
+                print("Warning: check the lensfit calibration in mean shear")
+                # TODO Someone using a lensft catalog needs to check
                 R_scalar[i], K[i], C[i], N[i] = cal.collect(self.comm)
-                sigma_e[i] /= 0.5*(R_scalar[i] + R_scalar[i])
-                mean_e1[i] = means[0] / R[i,0,0]
-                mean_e2[i] = means[1] / R[i,1,1]
-        
+                sigma_e[i] /= 0.5 * (R_scalar[i] + R_scalar[i])
+                # should probably use one of the calibration_tools functions
+                mean_e1[i] = means[0] / R_scalar[i]
+                mean_e2[i] = means[1] / R_scalar[i]
+            else:
+                raise ValueError("Unknown calibration type in mean g / sigma_e calc")
 
         if self.rank==0:
             if self.config['shear_catalog_type']=='metacal':