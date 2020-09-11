13d9fedcb5d574e1512995a951fbebb1b2b6b5f0
Eske M. Pedersen <emp160330@utdallas.edu>
2020-09-11 19:58:42 -0400

    Update twopoint.py

--- txpipe/twopoint.py
+++ txpipe/twopoint.py
@@ -1152,6 +1152,7 @@ class TXTwoPointGammaXPlots(TXTwoPointPlots):
         outputs = {
             "galaxy_shearDensity_xi_x": self.open_output('shearDensity_xi',
                 figsize=(3.5*nbin_lens, 3*nbin_source), wrapper=True)
+        }
         
         figures = {key: val.file for key, val in outputs.items()}
 
