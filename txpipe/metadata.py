b14d1a54a96a118c3629b599a5a857a65b785bfb
Joe Zuntz <joezuntz@googlemail.com>
2020-09-14 15:37:29 +0100

    Add 2D (non-tomographic) lens and calibrated source bins

--- txpipe/metadata.py
+++ txpipe/metadata.py
@@ -37,23 +37,38 @@ class TXTracerMetadata(PipelineStage):
         lens_tomo_file = self.open_input("lens_tomography_catalog")
 
         meta_file = self.open_output("tracer_metadata")
+        metadata = {}
 
         def copy(tomo, in_section, out_section, name):
             x = tomo[f"{in_section}/{name}"][:]
             meta_file.create_dataset(f"{out_section}/{name}", data=x)
+            metadata[name] = x.tolist()
 
         def copy_attrs(tomo, name, out_name):
             for k, v in tomo[name].attrs.items():
                 meta_file[out_name].attrs[k] = v
+                if isinstance(v, np.ndarray):
+                    v = v.tolist()
+                elif isinstance(v, np.float64):
+                    v = float(v)
+                elif isinstance(v, np.int64):
+                    v = int(v)
+                metadata[k] = v
 
         if shear_catalog_type == "metacal":
             copy(shear_tomo_file, "metacal_response", "tracers", "R_gamma_mean")
             copy(shear_tomo_file, "metacal_response", "tracers", "R_S")
             copy(shear_tomo_file, "metacal_response", "tracers", "R_total")
+            copy(shear_tomo_file, "metacal_response", "tracers", "R_gamma_mean_2d")
+            copy(shear_tomo_file, "metacal_response", "tracers", "R_S_2d")
+            copy(shear_tomo_file, "metacal_response", "tracers", "R_total_2d")
         else:
             copy(shear_tomo_file, "response", "tracers", "R_mean")
             copy(shear_tomo_file, "response", "tracers", "K")
             copy(shear_tomo_file, "response", "tracers", "C")
+            copy(shear_tomo_file, "response", "tracers", "R_mean_2d")
+            copy(shear_tomo_file, "response", "tracers", "K_2d")
+            copy(shear_tomo_file, "response", "tracers", "C_2d")
 
         copy(shear_tomo_file, "tomography", "tracers", "N_eff")
         copy(lens_tomo_file, "tomography", "tracers", "lens_counts")
@@ -62,54 +77,51 @@ class TXTracerMetadata(PipelineStage):
         copy(shear_tomo_file, "tomography", "tracers", "mean_e2")
         copy(shear_tomo_file, "tomography", "tracers", "source_counts")
 
+        copy(shear_tomo_file, "tomography", "tracers", "N_eff_2d")
+        copy(lens_tomo_file, "tomography", "tracers", "lens_counts_2d")
+        copy(shear_tomo_file, "tomography", "tracers", "sigma_e_2d")
+        copy(shear_tomo_file, "tomography", "tracers", "mean_e1_2d")
+        copy(shear_tomo_file, "tomography", "tracers", "mean_e2_2d")
+        copy(shear_tomo_file, "tomography", "tracers", "source_counts_2d")
+
         N_eff = shear_tomo_file["tomography/N_eff"][:]
+        N_eff_2d = shear_tomo_file["tomography/N_eff_2d"][:]
         n_eff = N_eff / area_sq_arcmin
+        n_eff_2d = N_eff_2d / area_sq_arcmin
 
         lens_counts = lens_tomo_file["tomography/lens_counts"][:]
         source_counts = shear_tomo_file["tomography/source_counts"][:]
+        lens_counts_2d = lens_tomo_file["tomography/lens_counts_2d"][:]
+        source_counts_2d = shear_tomo_file["tomography/source_counts_2d"][:]
 
         lens_density = lens_counts / area_sq_arcmin
         source_density = source_counts / area_sq_arcmin
+        lens_density_2d = lens_counts_2d / area_sq_arcmin
+        source_density_2d = source_counts_2d / area_sq_arcmin
 
         meta_file.create_dataset("tracers/n_eff", data=n_eff)
         meta_file.create_dataset("tracers/lens_density", data=lens_density)
         meta_file.create_dataset("tracers/source_density", data=source_density)
+        meta_file.create_dataset("tracers/lens_density_2d", data=lens_density_2d)
+        meta_file.create_dataset("tracers/source_density_2d", data=source_density_2d)
         meta_file["tracers"].attrs["area"] = area
         meta_file["tracers"].attrs["area_unit"] = "deg^2"
         meta_file["tracers"].attrs["density_unit"] = "arcmin^{-2}"
+        metadata['n_eff'] = n_eff.tolist()
+        metadata['n_eff_2d'] = n_eff_2d.tolist()
+        metadata['lens_density'] = lens_density.tolist()
+        metadata['source_density'] = source_density.tolist()
+        metadata['lens_density_2d'] = lens_density_2d.tolist()
+        metadata['source_density_2d'] = source_density_2d.tolist()
+        metadata['area'] = area
+        metadata['area_unit'] = "deg^2"
+        metadata['density_unit'] = "arcmin^{-2}"
         copy_attrs(shear_tomo_file, "tomography", "tracers")
         copy_attrs(lens_tomo_file, "tomography", "tracers")
-
         meta_file.close()
 
         # human readable version
         yaml_out_name = self.get_output("tracer_metadata_yml")
-        metadata = {
-            "lens_density": lens_density.tolist(),
-            "source_density": source_density.tolist(),
-            "sigma_e": shear_tomo_file["tomography/sigma_e"][:].tolist(),
-            "mean_e1": shear_tomo_file["tomography/mean_e1"][:].tolist(),
-            "mean_e2": shear_tomo_file["tomography/mean_e2"][:].tolist(),
-            "n_eff": n_eff.tolist(),
-            "lens_counts": lens_counts.tolist(),
-            "source_counts": source_counts.tolist(),
-            "area": float(area),
-            "area_unit": "deg^2",
-            "density_unit": "arcmin^{-2}",
-        }
-
-        if shear_catalog_type == "metacal":
-            metadata["R_gamma_mean"] = (
-                shear_tomo_file["metacal_response/R_gamma_mean"][:].tolist(),
-            )
-            metadata["R_S"] = (shear_tomo_file["metacal_response/R_S"][:].tolist(),)
-            metadata["R_total"] = (
-                shear_tomo_file["metacal_response/R_total"][:].tolist(),
-            )
-        else:
-            metadata["R_mean"] = (shear_tomo_file["response/R_mean"][:].tolist(),)
-            metadata["K"] = (shear_tomo_file["response/K"][:].tolist(),)
-            metadata["C"] = (shear_tomo_file["response/C"][:].tolist(),)
 
         f = open(yaml_out_name, "w")
         yaml.dump(metadata, f)
@@ -125,6 +137,6 @@ class TXTracerMetadata(PipelineStage):
 
         num_hit = (m > 0).sum()
         area_sq_deg = pixel_scheme.pixel_area(degrees=True) * num_hit
-        f_sky = area_sq_deg / 41252.96125
+        f_sky = float(area_sq_deg) / 41252.96125
         print(f"Area = {area_sq_deg:.2f} deg^2")
         return area_sq_deg
