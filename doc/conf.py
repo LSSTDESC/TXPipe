eb6b0df19e4ecc47460a23fcbb57a08708039b73
Eske M. Pedersen <emp160330@utdallas.edu>
2020-12-01 21:12:59 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -36,31 +36,23 @@ if os.path.isdir(root_path):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = [
-#     'sphinx.ext.autodoc',
-#     'sphinx.ext.todo',
-#     'sphinx.ext.mathjax',
-#     'sphinx.ext.viewcode',
-#     'sphinx.ext.ifconfig',
-#     'sphinx.ext.napoleon',
-#     'sphinx.ext.autosectionlabel',
-#     ]
-
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.ifconfig',
     'sphinx.ext.napoleon',
-]
+    'sphinx.ext.autosectionlabel',
+    ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
- source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
