487929a105f07e50b5e52dc4c142be9402efed05
Joe Zuntz <joezuntz@googlemail.com>
2021-03-09 22:49:07 +0000

    extend test

--- txpipe/test/test_utils.py
+++ txpipe/test/test_utils.py
@@ -2,10 +2,31 @@ from ..utils.misc import unique_list, hex_escape
 
 
 def test_escape():
-	assert hex_escape(chr(1))=='\\x01'
-	assert hex_escape(chr(10) + chr(11) + chr(12))=='\\x0a\\x0b\\x0c'
+    assert hex_escape(chr(1))=='\\x01'
+    assert hex_escape(chr(10) + chr(11) + chr(12))=='\\x0a\\x0b\\x0c'
 
 
 def test_unique_list():
-	a = unique_list([1, 2, 3, "a", "b", "a", 2, 0, 0, -1, ])
-	assert a == [1, 2, 3, "a", "b", 0, -1]
+
+    x = [1, 1, 1, 1]
+    assert unique_list(x) == [1]
+
+    x = [1, 1, 2, 1, 1]
+    assert unique_list(x) == [1, 2]
+
+    x = [1, 1, 2, 1, 1, 2, 3, 3]
+    assert unique_list(x) == [1, 2, 3]
+
+    x = [-1, "cat", -1, "cat", "dog"]
+    assert unique_list(x) == [-1, "cat", "dog"]
+
+def test_multi_where():
+    x = np.random.randint(0, 10, 100)
+    w = [1, 2, 3]
+
+    a = multi_where(x, w)
+
+    for m in w:
+        assert np.array_equal(np.where(x==m)[0], a[m])
+
+    assert a.keys() == set(w)
