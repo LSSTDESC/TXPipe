70cc24a1f2cbe82e4f36cbfb78286f2c3e5e3d17
Joe Zuntz <joezuntz@googlemail.com>
2021-06-02 10:29:25 +0100

    import and tweak healsparse mask loading from HSC LSS

--- txpipe/data_types/types.py
+++ txpipe/data_types/types.py
@@ -173,6 +173,10 @@ class MapsFile(HDFFile):
         if not pixel.shape == value.shape:
             raise ValueError(f"Map pixels and values should be same shape "
                              f"but are {pixel.shape} vs {value.shape}")
+
+        if 'maps' not in self.file.keys():
+            self.file.create_group('maps')
+
         subgroup = self.file['maps'].create_group(map_name)
         subgroup.attrs.update(metadata)
         subgroup.create_dataset("pixel", data=pixel)
--- txpipe/masks.py
+++ txpipe/masks.py
@@ -55,6 +55,83 @@ class TXSimpleMask(PipelineStage):
         mask = mask[pix].astype(float)
 
         with self.open_output("mask", wrapper=True) as f:
-
-            f.file.create_group("maps")
             f.write_map("mask", pix, mask, metadata)
+
+
+class TXHealsparseMask(PipelineStage):
+    """
+    Ingest a HealSparse map into TXPipe format.
+    """
+    name = "TXExternalMask"
+
+    # We use the auxiliary map suite (which contains depth maps and related quantities)
+    # just to choose get the Nside parameter.
+    inputs = [("aux_maps", MapsFile)]
+
+    outputs = [("mask", MapsFile)]
+
+    config_options = {
+        "mask_path": str,
+    }
+
+    def run(self):
+        import healpy
+        import healsparse
+
+
+        # Get the pixelization scheme
+        with self.open_input("aux_maps", wrapper=True) as f:
+            metadata = dict(f.file["maps"].attrs)
+            pixel_scheme = choose_pixelization(**metadata)
+
+        # Load the map, in this case from a Healsparse file
+        mask = self.read_map(pixel_scheme)
+
+        # In-place clip to positive values only, and remove nan. This also removes
+        # healpix UNSEEN values, which are negative
+        np.nan_to_num(mask, copy=False, nan=0, neginf=0, posinf=0)
+        np.clip(mask, 0, np.inf, out=mask)
+
+        # Convert to float64 type everywhere
+        pix = np.where(mask)[0]
+        mask = mask[pix].astype(float)
+
+        # Determinet the unmasked area, report and store it.
+        area = pixel_scheme.pixel_area(degrees=True) * pix.size
+        f_sky = area / 41252.96125
+        print(f"f_sky = {f_sky}")
+        print(f"area = {area:.2f} sq deg")
+        metadata["area"] = area
+        metadata["f_sky"] = f_sky
+
+        # Write to a mask file
+        with self.open_output("mask", wrapper=True) as f:
+            f.write_map("mask", pix, mask, metadata)
+
+
+    def read_map(self, pixel_scheme)
+        import healpy
+        import healsparse
+
+        # Read the 
+        hsp_mask = healsparse.HealSparseMap.read(self.config['mask_path'])
+        nside = hsp_mask.nside_sparse
+
+        # Complain if the external mask size is too small for what we
+        # need here
+        if pixel_scheme.nside > nside:
+            raise ValueError("Nside for the external sparse mask must be "
+                             "larger than nside for the object map")
+
+        # If needed, degrade the mask to our target resolution
+        if pixel_scheme.nside < nside:
+            hsp_mask = hsp_mask.degrade(nside_out=pixel_scheme.nside)
+
+        # Load the map.  The default Healsparse ordering is NEST, unlike many
+        # other uses, so we probably have to re-order from nest to ring here.
+        mask = hsp_mask.generate_healpix_map()
+        if not pixel_scheme.nest:
+            mask = healpy.reorder(mask, r2n=True)
+
+
+        return mask
