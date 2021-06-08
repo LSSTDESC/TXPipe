d97fe2af3b3a1e9cd6b08a71556c533f1f1e0f81
Eske M. Pedersen <emp160330@utdallas.edu>
2021-06-08 12:15:05 -0400

    Update random_cats_source.py

--- txpipe/extensions/random_cats_source.py
+++ txpipe/extensions/random_cats_source.py
@@ -1,6 +1,6 @@
-from .base_stage import PipelineStage
-from .data_types import MapsFile, YamlFile, RandomsCatalog, TomographyCatalog, HDFFile
-from .utils import choose_pixelization
+from ..base_stage import PipelineStage
+from ..data_types import MapsFile, YamlFile, RandomsCatalog, TomographyCatalog, HDFFile
+from ..utils import choose_pixelization
 import numpy as np
 
 
