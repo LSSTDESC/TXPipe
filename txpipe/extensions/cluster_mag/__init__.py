de0cedcfee0fdd90110228f05c51a3ed75c8c3a3
Joe Zuntz <jzuntz@cca001.in2p3.fr>
2021-07-12 15:50:15 +0200

    prevent namespace collision

--- txpipe/extensions/cluster_mag/__init__.py
+++ txpipe/extensions/cluster_mag/__init__.py
@@ -4,3 +4,6 @@ from .randoms import CMRandoms
 from .redshift import CMRedshifts
 from .correlations import CMCorrelations
 from .plots import CMCorrelationsPlot
+
+
+__all__ = ["CMSelectHalos", "CMSelectBackground", "CMIngestHalosCosmoDC2", "CMRandoms", "CMRedshifts", "CMCorrelations",  "CMCorrelationsPlot"]
