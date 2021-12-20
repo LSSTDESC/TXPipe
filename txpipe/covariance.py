38277d1e6f0a7a8fea5ddf804b535b2517f9928e
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2021-12-20 10:16:43 -0800

    nmtCov: added Barrier

--- txpipe/covariance.py
+++ txpipe/covariance.py
@@ -645,13 +645,16 @@ class TXFourierTJPCovariance(PipelineStage):
         # Load NmtBin used for the Cells
         workspaces = self.get_workspaces_dict(cl_sacc, masks_names)
 
+        # MPI
+        if self.comm:
+            self.comm.Barrier()
+            tjp_config['use_mpi'] = True
+
         # Run TJPCov
         # I shouldn't need to pass the binnning (unless the cache is not set or
         # there is one of the workspaces missing). For generality, I will pass
         # it.
         tjp_config['binning_info'] = self.recover_NmtBin(cl_sacc)
-        # MPI
-        tjp_config['use_mpi'] = True if self.comm is not None else False
         calculator = tjpcov.main.CovarianceCalculator({"tjpcov":tjp_config})
 
         cache = {'workspaces': workspaces}
