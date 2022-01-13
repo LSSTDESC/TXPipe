0280d7df7a8f017fcac63751ec964216e440c4b0
jpratmarti <jpratmarti@gmail.com>
2022-01-13 05:35:59 -0800

    Adding the option to mock a metacal catalog. Copying some older metacal functions.

--- txpipe/input_cats.py
+++ txpipe/input_cats.py
@@ -38,6 +38,7 @@ class TXCosmoDC2Mock(PipelineStage):
         'flip_g2': True, # this matches the metacal definition, and the treecorr/namaster one
         'apply_mag_cut': False, #used when comparing to descqa measurements
         'Mag_r_limit': -19, # used to decide what objects to cut out
+        'metadetect': True, # Alternatively we will mock a  metacal catalog
         }
 
     def data_iterator(self, gc):
@@ -103,7 +104,10 @@ class TXCosmoDC2Mock(PipelineStage):
         metacal_file = self.open_output('shear_catalog', parallel=self.is_mpi())
         photo_file = self.open_output('photometry_catalog', parallel=self.is_mpi())
         photo_cols = self.setup_photometry_output(photo_file, target_size)
-        metacal_cols = self.setup_metadetect_output(metacal_file, target_size)
+        if self.config['metadetect']:
+            metacal_cols = self.setup_metadetect_output(metacal_file, target_size)
+        else:
+            metacal_cols = self.setup_metacal_output(metacal_file, target_size)
 
 
         # Load the metacal response file
@@ -283,6 +287,35 @@ class TXCosmoDC2Mock(PipelineStage):
         return cols + ['id',  'mcal_flags']
         
 
+    def setup_metacal_output(self, metacal_file, target_size):
+        # Get a list of all the column names
+        cols = (
+            ['ra', 'dec', 'psf_g1', 'psf_g2', 'mcal_psf_g1', 'mcal_psf_g2', 'mcal_psf_T_mean']
+            + metacal_variants('mcal_g1', 'mcal_g2', 'mcal_T', 'mcal_s2n',  'mcal_T_err')
+            + band_variants('riz', 'mcal_mag', 'mcal_mag_err',shear_catalog_type='metacal')
+            + ['weight']
+        )
+
+        cols += ['true_g1', 'true_g2', 'redshift_true']
+
+        # Make group for all the photometry
+        group = metacal_file.create_group('shear')
+
+        # Extensible columns becase we don't know the size yet.
+        # We will cut down the size at the end.
+        
+
+        for col in cols:
+            group.create_dataset(col, (target_size,), maxshape=(target_size,), dtype='f8')
+
+        group.create_dataset('id', (target_size,), maxshape=(target_size,), dtype='i8')
+        group.create_dataset('mcal_flags', (target_size,), maxshape=(target_size,), dtype='i8')
+
+
+        return cols + ['id',  'mcal_flags']
+       
+
+    
     def load_metacal_response_model(self):
         """
         Load an HDF file containing the response model
@@ -360,9 +393,10 @@ class TXCosmoDC2Mock(PipelineStage):
 
 
 
-    def make_mock_metacal(self, data, photo):
+    def make_mock_metadetect(self, data, photo):
         """
-        Generate a mock metacal table.
+        !!! This function is not working properly yet!!!
+        Generate a mock metadetect table.
         This is a long and complicated function unfortunately.
 
         Throughout we assume a single R = R11 = R22, with R12 = R21 = 0
@@ -562,6 +596,209 @@ class TXCosmoDC2Mock(PipelineStage):
 
         return output
 
+    def make_mock_metacal(self, data, photo):
+        """
+        I copied the old version here to use if we do not want metadetect.
+        Generate a mock metacal table.
+        This is a long and complicated function unfortunately.
+        Throughout we assume a single R = R11 = R22, with R12 = R21 = 0
+        
+        Parameters
+        ----------
+        data: dict
+            Dictionary of arrays read from the input cosmo catalog
+        photo: dict
+            Dictionary of arrays generated to simulate photometry
+        """
+
+
+        # These are the numbers from figure F1 of the DES Y1 shear catalog paper
+        # (this version is not yet public but is awaiting a second referee response)
+
+        # Overall SNR for the three bands usually used for shape measurement
+        # We use the true SNR not the estimated one, though these are pretty close
+        snr = (
+            photo['snr_r']**2 + 
+            photo['snr_i']**2 + 
+            photo['snr_z']**2
+            )**0.5
+        snr_1p = (photo['snr_r_1p']**2 + photo['snr_i_1p']**2 + photo['snr_z_1p']**2)**0.5
+        snr_1m = (photo['snr_r_1m']**2 + photo['snr_i_1m']**2 + photo['snr_z_1m']**2)**0.5
+        snr_2p = (photo['snr_r_2p']**2 + photo['snr_i_2p']**2 + photo['snr_z_2p']**2)**0.5
+        snr_2m = (photo['snr_r_2m']**2 + photo['snr_i_2m']**2 + photo['snr_z_2m']**2)**0.5
+
+        if self.config['unit_response']:
+            assert np.allclose(snr, snr_1p)
+            assert np.allclose(snr, snr_2m)
+        
+        nobj = snr.size
+
+        log10_snr = np.log10(snr)
+
+        # Convert from the half-light radius which is in the 
+        # input catalogs to a sigma.  Do this by pretending
+        # that it is a Gaussian.  This is clearly wrong, and
+        # if this causes major errors in the size cuts we may
+        # have to modify this
+        size_hlr = data['size_true']
+        size_sigma = size_hlr / np.sqrt(2*np.log(2))
+        size_T = 2 * size_sigma**2
+
+        # Use a fixed PSF across all the objects
+        psf_fwhm = 0.75
+        psf_sigma = psf_fwhm/(2*np.sqrt(2*np.log(2)))
+        psf_T = 2 * psf_sigma**2
+
+        if self.config['unit_response']:
+            R = 1.0
+            R_size = 0.0
+
+        else:
+            # Use the response model to get a reasonable response
+            # value for this size and SNR
+            R_mean = self.R_spline(log10_snr, size_T, grid=False)
+            R_std = self.Rstd_spline(log10_snr, size_T, grid=False)
+            
+            # Assume a 0.2 correlation between the size response
+            # and the shear response.
+            rho = 0.2
+            f = np.random.multivariate_normal([0.0,0.0], [[1.0,rho],[rho,1.0]], nobj).T
+            R, R_size = f * R_std + R_mean
+
+        # Convert magnitudes to fluxes according to the baseline
+        # use in the metacal numbers
+        flux_r = 10**0.4*(27 - photo['mag_r'])
+        flux_i = 10**0.4*(27 - photo['mag_i'])
+        flux_z = 10**0.4*(27 - photo['mag_z'])
+
+        # Note that this is delta_gamma not 2*delta_gamma, because
+        # of how we use it below
+        delta_gamma = 0.01
+        
+        # Use a fixed shape noise per component to generate 
+        # an overall 
+        shape_noise = 0.26
+        eps  = np.random.normal(0,shape_noise,nobj) + 1.j * np.random.normal(0,shape_noise,nobj)
+        # True shears without shape noise
+        g1 = data['shear_1']
+        g2 = data['shear_2']
+
+        if self.config['flip_g2']:
+            g2 *= -1
+
+        # Do the full combination of (g,epsilon) -> e, not the approx one
+        g = g1 + 1j*g2
+        e = (eps + g) / (1+g.conj()*eps)
+        e1 = e.real
+        e2 = e.imag
+    
+        zero = np.zeros(nobj)
+        # Now collect together everything to go into the metacal
+        # file
+        output = {
+            # Basic values
+            'id': photo['id'],
+            'ra': photo['ra'],
+            'dec': photo['dec'],
+            # Keep the truth value just in case
+            "true_g1": g1,
+            "true_g2": g2,
+            # add true redshift since it is used in source selector
+            "redshift_true":photo['redshift_true'],
+            
+
+            # g1
+            "mcal_g1": e1*R,
+            "mcal_g1_1p": (e1+delta_gamma)*R,
+            "mcal_g1_1m": (e1-delta_gamma)*R,
+            "mcal_g1_2p": e1*R,
+            "mcal_g1_2m": e1*R,
+
+            # g2
+            "mcal_g2": e2*R,
+            "mcal_g2_1p": e2*R,
+            "mcal_g2_1m": e2*R,
+            "mcal_g2_2p": (e2+delta_gamma)*R,
+            "mcal_g2_2m": (e2-delta_gamma)*R,
+
+            # T
+            "mcal_T": size_T,
+            "mcal_T_1p": size_T + R_size*delta_gamma,
+            "mcal_T_1m": size_T - R_size*delta_gamma,
+            "mcal_T_2p": size_T + R_size*delta_gamma,
+            "mcal_T_2m": size_T - R_size*delta_gamma,
+
+            # Terr
+            "mcal_T_err":    zero,
+            "mcal_T_err_1p": zero,
+            "mcal_T_err_1m": zero,
+            "mcal_T_err_2p": zero,
+            "mcal_T_err_2m": zero,
+
+            # size 
+            "mcal_s2n": snr,
+            "mcal_s2n_1p": snr_1p,
+            "mcal_s2n_1m": snr_1m,
+            "mcal_s2n_2p": snr_2p,
+            "mcal_s2n_2m": snr_2m,
+
+            # Magntiudes and fluxes, just copied from the inputs.
+            'mcal_mag_r': photo['mag_r'],
+            'mcal_mag_i': photo['mag_i'],
+            'mcal_mag_z': photo['mag_z'],
+
+            'mcal_mag_err_r': photo['mag_r_err'],
+            'mcal_mag_err_i': photo['mag_i_err'],
+            'mcal_mag_err_z': photo['mag_z_err'],
+
+            'mcal_mag_r_1p': photo['mag_r_1p'],
+            'mcal_mag_r_2p': photo['mag_r_2p'],
+            'mcal_mag_r_1m': photo['mag_r_1m'],
+            'mcal_mag_r_2m': photo['mag_r_2m'],
+
+            'mcal_mag_i_1p': photo['mag_i_1p'],
+            'mcal_mag_i_2p': photo['mag_i_2p'],
+            'mcal_mag_i_1m': photo['mag_i_1m'],
+            'mcal_mag_i_2m': photo['mag_i_2m'],
+            
+            'mcal_mag_z_1p': photo['mag_z_1p'],
+            'mcal_mag_z_2p': photo['mag_z_2p'],
+            'mcal_mag_z_1m': photo['mag_z_1m'],
+            'mcal_mag_z_2m': photo['mag_z_2m'],
+
+            'mcal_mag_err_r_1p': photo['mag_r_err'],
+            'mcal_mag_err_r_2p': photo['mag_r_err'],
+            'mcal_mag_err_r_1m': photo['mag_r_err'],
+            'mcal_mag_err_r_2m': photo['mag_r_err'],
+
+            'mcal_mag_err_i_1p': photo['mag_i_err'],
+            'mcal_mag_err_i_2p': photo['mag_i_err'],
+            'mcal_mag_err_i_1m': photo['mag_i_err'],
+            'mcal_mag_err_i_2m': photo['mag_i_err'],
+
+            'mcal_mag_err_z_1p': photo['mag_z_err'],
+            'mcal_mag_err_z_2p': photo['mag_z_err'],
+            'mcal_mag_err_z_1m': photo['mag_z_err'],
+            'mcal_mag_err_z_2m': photo['mag_z_err'],
+
+            # Fixed PSF parameters - all round with same size
+            'mcal_psf_g1': zero,
+            'mcal_psf_g2': zero,
+            'psf_g1': zero,
+            'psf_g2': zero,
+            'mcal_psf_T_mean' : np.repeat(psf_T, nobj),
+
+            # Everything that gets this far should be used, so flag=0
+            'mcal_flags': np.zeros(nobj, dtype=np.int32),
+            # we use weights of one for everything for metacal
+            # if that ever changes we may also need to add
+            # weight_1p, etc.
+            'weight': np.ones(nobj),
+            }
+
+        return output
+
+    
     def apply_magnitude_cut(self, data):
         """
         Allow for a cut in absolute magnitude.
@@ -700,6 +937,8 @@ class TXGaussianSimsMock(TXCosmoDC2Mock):
         'cat_size': 0,
         'flip_g2': False, # this matches the metacal definition, and the treecorr/namaster one
         'apply_mag_cut': False, #used when comparing to descqa measurements
+        'metadetect': False, # we will mock a  metacal catalog
+        
         }
 
     def data_iterator(self, cat):
@@ -875,6 +1114,8 @@ def make_mock_photometry(n_visit, bands, data, unit_response):
 
 
 
+
+
 def generate_mock_metacal_mag_responses(bands, nobj):
     nband = len(bands)
     mu = np.zeros(nband) # seems approx mean of response across bands, from HSC tract
@@ -903,3 +1144,7 @@ def test():
     results = make_mock_photometry(n_visit, bands, data, True)
     pylab.hist(results['snr_r'], bins=50, histtype='step')
     pylab.savefig('snr_r.png')
+
+
+
+    
