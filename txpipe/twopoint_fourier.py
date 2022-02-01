d635fd6892a85673eceb136f65bc6cf5527b1f1b
jpratmarti <jpratmarti@gmail.com>
2022-02-01 13:35:57 -0800

    Cleanup.

--- txpipe/twopoint_fourier.py
+++ txpipe/twopoint_fourier.py
@@ -552,15 +552,15 @@ class TXTwoPointFourier(PipelineStage):
         if self.config['analytic_noise']:
             # we are going to subtract the noise afterwards
             c = nmt.compute_full_master(field_i, field_j, ell_bins,
-                                    cl_guess=cl_guess, workspace=workspace, n_iter=1)
-             # noise to subtract (already decoupled)
-             cl_noise = self.compute_noise_analytic(i, j, k, maps, f_sky, workspace)
-             if cl_noise is not None:
-                 c = c - cl_noise
-                 # Writing out the noise for later cross-checks
+                                        cl_guess=cl_guess, workspace=workspace, n_iter=1)
+            # noise to subtract (already decoupled)
+            cl_noise = self.compute_noise_analytic(i, j, k, maps, f_sky, workspace)
+            if cl_noise is not None:
+                c = c - cl_noise
+                # Writing out the noise for later cross-checks
             
         else:
-            #Get the coupled noise C_ell values to give to the master algorithm
+            # Get the coupled noise C_ell values to give to the master algorithm
             cl_noise = self.compute_noise(i, j, k, ell_bins, maps, workspace)
             c = nmt.compute_full_master(field_i, field_j, ell_bins,
                                         cl_noise=cl_noise, cl_guess=cl_guess, workspace=workspace, n_iter=1)
