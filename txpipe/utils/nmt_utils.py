78bfb80729c1061925e63e524e2ee5d40163e556
fjaviersanchez <francs1@uci.edu>
2022-02-24 13:48:43 -0800

    fix to be able to use updates

--- txpipe/utils/nmt_utils.py
+++ txpipe/utils/nmt_utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import healpy
 import pathlib
 
+
 class MyNmtBinFlat(nmt.NmtBinFlat):
     def __init__(self, l0, lf):
         super().__init__(l0, lf)
@@ -10,7 +11,7 @@ class MyNmtBinFlat(nmt.NmtBinFlat):
         self.ell_max = lf
 
     def get_window(self, b):
-        ell = np.arange(self.ell_min[b], self.ell_max[b]+1)
+        ell = np.arange(self.ell_min[b], self.ell_max[b] + 1)
         w = np.ones_like(ell)
         return (ell, w)
 
@@ -25,11 +26,31 @@ class MyNmtBinFlat(nmt.NmtBinFlat):
 
     def apply_window(self, b, c_ell):
         b0, b1 = self.get_window(b)
-        return c_ell[b0:b1+1].mean()
+        return c_ell[b0 : b1 + 1].mean()
+
 
 class MyNmtBin(nmt.NmtBin):
-    def __init__(self, nside=None, bpws=None, ells=None, weights=None, nlb=None, lmax=None, is_Dell=False, f_ell=None):
-        super().__init__(nside=nside, bpws=bpws, ells=ells, weights=weights, nlb=nlb, lmax=lmax, is_Dell=False, f_ell=None)
+    def __init__(
+        self,
+        nside=None,
+        bpws=None,
+        ells=None,
+        weights=None,
+        nlb=None,
+        lmax=None,
+        is_Dell=False,
+        f_ell=None,
+    ):
+        super().__init__(
+            nside=nside,
+            bpws=bpws,
+            ells=ells,
+            weights=weights,
+            nlb=nlb,
+            lmax=lmax,
+            is_Dell=False,
+            f_ell=None,
+        )
         self.ell_max = self.lmax
 
     def get_window(self, b):
@@ -48,11 +69,19 @@ class MyNmtBin(nmt.NmtBin):
 
     def apply_window(self, b, c_ell):
         ell, weight = self.get_window(b)
-        return (c_ell[ell]*weight).sum() / weight.sum()
+        return (c_ell[ell] * weight).sum() / weight.sum()
 
+    @classmethod
+    def from_binning_info(cls, ell_min, ell_max, n_ell, ell_spacing):
+        # Creating the ell binning from the edges using this Namaster constructor.
+        if ell_spacing == "log":
+            edges = np.unique(np.geomspace(ell_min, ell_max, n_ell).astype(int))
+        else:
+            edges = np.unique(np.linspace(ell_min, ell_max, n_ell).astype(int))
 
-import healpy
+        ell_bins = cls.from_edges(edges[:-1], edges[1:], is_Dell=False)
 
+        return ell_bins
 
 
 class WorkspaceCache:
@@ -65,7 +94,7 @@ class WorkspaceCache:
         if key in self._loaded:
             return self._loaded[key]
 
-        p = self.path / f'workspace_{key}.dat'
+        p = self.get_path(key)
 
         if not p.exists():
             return None
@@ -79,9 +108,12 @@ class WorkspaceCache:
 
         return workspace
 
+    def get_path(self, key):
+        return self.path / f"workspace_{key}.dat"
+
     def put(self, workspace):
         key = workspace.txpipe_key
-        p = self.path / f'workspace_{key}.dat'
+        p = self.get_path(key)
         if p.exists():
             return False
 
