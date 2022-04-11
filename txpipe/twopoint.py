7fda220a20f30ebbdc002be1edc90b1f7ed400d4
jpratmarti <jpratmarti@gmail.com>
2022-04-11 11:14:56 -0700

    Cleaning up.

--- txpipe/twopoint.py
+++ txpipe/twopoint.py
@@ -95,8 +95,6 @@ class TXTwoPoint(PipelineStage):
 
         # Choose which pairs of bins to calculate
         calcs = self.select_calculations(source_list, lens_list)
-        #print('Hacking number of correlations!!!')
-        #calcs = [(4, 4, POS_POS)]
         sys.stdout.flush()
 
         # Split the catalogs into patch files
