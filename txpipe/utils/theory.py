9f381883cb59cafd206c135a0177db86d0b461ad
Joe Zuntz <joezuntz@googlemail.com>
2022-11-28 15:27:00 +0000

    Move all theory calculations to new firecrown

--- txpipe/utils/theory.py
+++ txpipe/utils/theory.py
@@ -51,111 +51,156 @@ def ccl_read_yaml(filename, **kwargs):
     return ccl.Cosmology(**inits)
 
 
-def theory_3x2pt(cosmology_file, tracers, nbin_source, nbin_lens, fourier=True):
-    """Compute the 3x2pt theory, for example for a fiducial cosmology
+
+def theory_3x2pt(cosmo, sacc_data, bias=None, smooth=False):
+    """
+    Use FireCrown to generate the theory predictions for the data
+    in a sacc file.
+
+    Linear galaxy bias parameters can optionally be provided as an array.
+    If left as None they are set to unity.
 
     Parameters
     ----------
-    cosmology_file: str
-        name of YAML file
-
-    tracers: dict{str:obj}
-        dict of objects (e.g. sacc tracers) containing z, nz.
-        keys are source_0, source_1, ..., lens_0, lens_1, ...
-
-    nbin_source: int
-        number of source bins
-
-    nbin_lens: int
-        number of lens bins
+    cosmo: pyccl.Cosmology
+        The cosmology at which to get the theory predictions
+    sacc_data: sacc.Sacc or str
+        Sacc object or a path to one
+    galaxy_bias: float, array, or None
+        If a float b_0, use the scheme b = b_0 / D(<z>) for each bin.
+        If an array, use the given value for each bin
 
     Returns
     -------
-    theory_cl: dict{str:array}
-        theory c_ell for each pair (i,j,k) where k is one of
-        SHEAR_SHEAR = 0, SHEAR_POS = 1, POS_POS = 2
+    sacc_theory: sacc.Sacc
+        A copy of the input sacc_data but with values
+        replaced with theory predictions.
+    """
+    import firecrown.likelihood.gauss_family.statistic.source.weak_lensing as wl
+    import firecrown.likelihood.gauss_family.statistic.source.number_counts as nc
+    from firecrown.likelihood.gauss_family.statistic.two_point import TwoPoint
+    import sacc
+    import pathlib
 
+
+    if isinstance(sacc_data, (str, pathlib.Path)):
+        sacc_data = sacc.Sacc.load_fits(sacc_data)
+
+    if smooth:
+        sacc_data = smooth_sacc_nz(sacc_data)
+
+    # breakpoint()
+
+    if isinstance(bias, float):
+        bias = compute_fiducial_bias(bias, sacc_data, cosmo)
+    elif bias is None:
+        bias = [1 for _ in sacc_data.tracers]
+        print("Using bias = 1")
+    else:
+        print("Bias = ", bias)
+
+    # Creating sources. This will presumably be done inside
+    # firecrown at some point
+    sources = {}
+    for tracer_name in sacc_data.tracers.keys():
+        if tracer_name.startswith("source"):
+            tr = wl.WeakLensing(sacc_tracer=tracer_name)
+        elif tracer_name.startswith("lens"):
+            tr = nc.NumberCounts(sacc_tracer=tracer_name)
+            i = int(tracer_name.split('_')[1])
+            tr.bias = bias[i]
+            tr.mag_bias = 0.0
+        else:
+            raise ValueError(f"Unknown tracer in sacc file, non-3x2pt: {tracer}")
+        sources[tracer_name] = tr
+
+    # Making predictions using the TwoPoint statistic object
+    # from firecrown
+    theory = {}
+    for dt in sacc_data.get_data_types():
+        for t1, t2 in sacc_data.get_tracer_combinations(dt):
+            try:
+                stat = TwoPoint(
+                        source0=sources[t1],
+                        source1=sources[t2],
+                        sacc_data_type=dt,
+                )
+            except ValueError as err:
+                # B modes and things like that can be in the files
+                # but are not supported
+                if "is not supported" in str(err):
+                    print(f"Setting bin {t1}, {t2} for {dt} to zero")
+                    n = sacc_data.indices(dt, (t1,t2)).size
+                    theory[dt, t1, t2] = np.zeros(n)
+                    continue
+                else:
+                    raise
+
+            # Read statistic information from the sacc file
+            stat.read(sacc_data)
+
+            # The first output is the data vector, and
+            # we keep the second which is the theory
+            _, theory[dt, t1, t2] = stat.compute(cosmo)
+        
+
+    # Put the theory values into the new sacc object
+    # We really need a set_mean on sacc objects to do this.
+    sacc_theory = sacc_data.copy()
+    for dt in sacc_theory.get_data_types():
+        for t1, t2 in sacc_theory.get_tracer_combinations(dt):
+            th = theory[dt, t1, t2]
+            index = sacc_theory.indices(dt, (t1,t2))
+            assert len(index) == len(th)
+            for i,j in enumerate(index):
+                sacc_theory.data[j].value = th[i]
+
+    return sacc_theory
+
+    
+def compute_fiducial_bias(b0, sack, cosmo):
+    """
+    Return an array of bias values following b[i] = b_0 / D(<z_i>)
+
+    Parameters
+    ----------
+    b0: float
+
+    sack: Sacc
+
+    cosmo: pyccl.Cosmology
     """
     import pyccl as ccl
 
-    cosmo = ccl_read_yaml(cosmology_file, matter_power_spectrum="halofit", Neff=3.04)
+    print("Computing bias values b(z)")
+    # now get bias as a function of redshift for each lens bin
+    bias = {}
+    for key, tracer in sack.tracers.items():
+        if "lens" in key:
+            i = int(key.split('_')[1])
+            z_eff = (tracer.z * tracer.nz).sum() / tracer.nz.sum()
+            a_eff = 1 / (1 + z_eff)
+            bias[i] = b0 / ccl.growth_factor(cosmo, a_eff)
+    bias = np.array([bias[i] for i in range(len(bias))])
+    print("Found bias:", bias)
+    return bias
 
-    ell_max = 3000 if fourier else 100_000
-    n_ell = 100 if fourier else 200
-    ell = np.logspace(1, np.log10(ell_max), n_ell).astype(int)
-    ell = np.unique(ell)
 
-    # Convert from SACC tracers (which just store N(z))
-    # to CCL tracers (which also have cosmology info in them).
-    CTracers = {}
+def smooth_sacc_nz(sack):
+    """
+    Smooth each n(z) in a sacc object, in-place.
 
-    # Lensing tracers - need to think a little more about
-    # the fiducial intrinsic alignment here
-    for i in range(nbin_source):
-        x = tracers[f"source_{i}"]
-        tag = ("S", i)
-        CTracers[tag] = ccl.WeakLensingTracer(cosmo, dndz=(x.z, x.nz))
+    Parameters
+    ----------
+    sacc: Sacc
 
-    # Position tracers - even more important to think about fiducial biases
-    # here - these will be very very wrong otherwise!
-    # Important enough that I'll put in a warning.
-    warnings.warn("Not using galaxy bias in fiducial theory density spectra")
+    Returns
+    -------
+    None
+    """
 
-    for i in range(nbin_lens):
-        x = tracers[f"lens_{i}"]
-        tag = ("P", i)
-        b = np.ones_like(x.z)
-        CTracers[tag] = ccl.NumberCountsTracer(
-            cosmo, dndz=(x.z, x.nz), has_rsd=False, bias=(x.z, b)
-        )
+    for key, tracer in sack.tracers.items():
+        tracer.nz = smooth_nz(tracer.nz)
 
-    # Use CCL to actually calculate the C_ell values for the different cases
-    theory_cl = {}
-    theory_cl["ell"] = ell
-    k = SHEAR_SHEAR
-    for i in range(nbin_source):
-        for j in range(i + 1):
-            Ti = CTracers[("S", i)]
-            Tj = CTracers[("S", j)]
-            # The full theory C_ell over the range 0..ellmax
-            theory_cl[(i, j, k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
-            theory_cl[(j, i, k)] = theory_cl[(i, j, k)]
-
-    # The same for the galaxy galaxy-lensing cross-correlation
-    k = SHEAR_POS
-    for i in range(nbin_source):
-        for j in range(nbin_lens):
-            Ti = CTracers[("S", i)]
-            Tj = CTracers[("P", j)]
-            theory_cl[(i, j, k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
-
-    # And finally for the density correlations
-    k = POS_POS
-    for i in range(nbin_lens):
-        for j in range(i + 1):
-            Ti = CTracers[("P", i)]
-            Tj = CTracers[("P", j)]
-            theory_cl[(i, j, k)] = ccl.angular_cl(cosmo, Ti, Tj, ell)
-            theory_cl[(j, i, k)] = theory_cl[(i, j, k)]
-
-    if fourier:
-        return theory_cl
-
-    theta_min = 1.0 / 60
-    theta_max = 3.0
-    theta = np.logspace(np.log10(theta_min), np.log10(theta_max), 200)
-
-    theory_xi = {"theta": theta * 60}  # arcmin
-    for key, val in theory_cl.items():
-        if key == "ell":
-            continue
-        i, j, k = key
-        corr_type = {SHEAR_SHEAR: "L+", POS_POS: "GG", SHEAR_POS: "GL"}[k]
-        xi = ccl.correlation(cosmo, ell, val, theta, corr_type=corr_type)
-        if k == SHEAR_SHEAR:
-            xim = ccl.correlation(cosmo, ell, val, theta, corr_type="L-")
-            theory_xi[(i, j, k)] = [xi, xim]
-        else:
-            theory_xi[(i, j, k)] = xi
-
-    return theory_cl, theory_xi
+def smooth_nz(nz):
+    return np.convolve(nz, np.exp(-0.5 * np.arange(-4, 5) ** 2) / 2**2, mode="same")
