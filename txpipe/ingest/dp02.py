04d6dd5027fe80974e90b3a6ab84c5cc879e558d
Joe Zuntz <joezuntz@googlemail.com>
2023-03-24 14:17:23 +0000

    add psf values to output

--- txpipe/ingest/dp02.py
+++ txpipe/ingest/dp02.py
@@ -1,6 +1,6 @@
 from ..base_stage import PipelineStage
 from ..data_types import ShearCatalog, HDFFile
-from ..utils import band_variants, metacal_variants, nanojansky_err_to_mag_ab, nanojansky_to_mag_ab
+from ..utils import band_variants, metacal_variants, nanojansky_err_to_mag_ab, nanojansky_to_mag_ab, moments_to_shear
 import numpy as np
 import glob
 import re
@@ -175,22 +175,29 @@ class TXIngestDataPreview02(PipelineStage):
             f_err = data[f"{band}_cModelFluxErr"][cut]
             output[f'mag_{band}'] = nanojansky_to_mag_ab(f)
             output[f'mag_err_{band}'] = nanojansky_err_to_mag_ab(f, f_err)
+
+            if band == "i":
+                output['s2n'] = f / f_err
         
         output["g1"] = data['i_hsmShapeRegauss_e1'][cut]
         output["g2"] = data['i_hsmShapeRegauss_e2'][cut]
         output["T"] = data['i_ixx'][cut] + data['i_iyy'][cut]
         output["flags"] = data["i_hsmShapeRegauss_flag"][cut]
-        output['s2n'] = f / f_err
+
 
         # Fake numbers! These need to be derived from simulation.
         # In this case 
         output["m"] = np.repeat(-1.184445e-01, f.size)
         output["c1"] = np.repeat(-2.316957e-04, f.size)
         output["c2"] = np.repeat(-8.629799e-05, f.size)
-
         output["sigma_e"] = np.repeat(1.342084e-01, f.size)
         output["weight"] = np.ones_like(f)
+
+        # PSF components
         output["psf_T_mean"] = data['i_ixxPSF'][cut] + data['i_iyyPSF'][cut]
+        psf_g1, psf_g2 = moments_to_shear(data['i_ixxPSF'][cut], data['i_iyyPSF'][cut], data['i_ixyPSF'][cut])
+        output["psf_g1"] = psf_g1
+        output["psf_g2"] = psf_g2
 
             
             
