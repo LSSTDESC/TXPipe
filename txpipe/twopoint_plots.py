a1e74573501aec27659d1ddd8c135895ff536748
jjeffe14 <jeffersonjazmine@gmail.com>
2023-05-10 11:50:57 -0700

    fixing twopointplots

--- txpipe/twopoint_plots.py
+++ txpipe/twopoint_plots.py
@@ -1,6 +1,6 @@
 
 from .base_stage import PipelineStage
-from .data_types import SACCFile, PNGFile
+from .data_types import FiducialCosmology, SACCFile, PNGFile
 import numpy as np
 
 
@@ -14,7 +14,9 @@ class TXTwoPointPlots(PipelineStage):
     parallel = False
     inputs = [
         ("twopoint_data_real", SACCFile),
+        ("fiducial_cosmology", FiducialCosmology),  # For example lines
         ("twopoint_gamma_x", SACCFile),
+        ("twopoint_theory_real", SACCFile),
     ]
     outputs = [
         ("shear_xi_plus", PNGFile),
@@ -72,11 +74,24 @@ class TXTwoPointPlots(PipelineStage):
             [filename],
             ["twopoint_data_real"],
             figures=figures,
+            theory_sacc_files=[filename_theory],
+            theory_labels=["Fiducial"],
         )
 
         for fig in outputs.values():
             fig.close()
 
+        full_3x2pt_plots(
+            [filename],
+            ["twopoint_data_real"],
+            figures=figures,
+            theory_sacc_files=[filename_theory],
+            theory_labels=["Fiducial"],
+            ratios=True,
+        )
+
+        for fig in outputs.values():
+            fig.close()
 
         filename = self.get_input("twopoint_gamma_x")
 
@@ -124,6 +139,7 @@ class TXTwoPointPlotsFourier(PipelineStage):
     parallel = False
     inputs = [
         ("summary_statistics_fourier", SACCFile),
+        ("fiducial_cosmology", FiducialCosmology),  # For example lines
         ("twopoint_theory_fourier", SACCFile),
     ]
     outputs = [
@@ -239,6 +255,7 @@ class TXTwoPointPlotsTheory( TXTwoPointPlots ):
     parallel = False
     inputs = [
         ("twopoint_data_real", SACCFile),
+        ("fiducial_cosmology", FiducialCosmology),  # For example lines
         ("twopoint_gamma_x", SACCFile),
         ("twopoint_theory_real", SACCFile),
     ]
