92b3426a2841181ecd3fc49b163658a4d44b9735
Joe Zuntz <joezuntz@googlemail.com>
2023-07-04 13:01:05 +0100

    Add metadetect variant with ony source, no lens

--- txpipe/metadata.py
+++ txpipe/metadata.py
@@ -5,6 +5,21 @@ from .data_types import TomographyCatalog, MapsFile, HDFFile, YamlFile, ShearCat
 from .utils.calibration_tools import read_shear_catalog_type
 from .utils import choose_pixelization
 
+def copy(tomo, in_section, out_section, name, meta_file, metadata):
+    x = tomo[f"{in_section}/{name}"][:]
+    meta_file.create_dataset(f"{out_section}/{name}", data=x)
+    metadata[name] = x.tolist()
+
+def copy_attrs(tomo, name, out_name, meta_file, metadata):
+    for k, v in tomo[name].attrs.items():
+        meta_file[out_name].attrs[k] = v
+        if isinstance(v, np.ndarray):
+            v = v.tolist()
+        elif isinstance(v, np.float64):
+            v = float(v)
+        elif isinstance(v, np.int64):
+            v = int(v)
+        metadata[k] = v
 
 class TXTracerMetadata(PipelineStage):
     """
@@ -30,112 +45,113 @@ class TXTracerMetadata(PipelineStage):
         ("tracer_metadata_yml", YamlFile),  # human-readable version
     ]
 
+    def copy_source_metadata(self, meta_file, metadata, area, area_sq_arcmin):
+        if self.get_input("shear_tomography_catalog") == "none":
+            print("Skipping source metadata")
+            return
+        shear_catalog_type = read_shear_catalog_type(self)
+
+        with self.open_input("shear_tomography_catalog") as shear_tomo_file:
+            if shear_catalog_type == "metacal":
+                copy(shear_tomo_file, "response", "tracers", "R_gamma_mean", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_S", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_total", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_gamma_mean_2d", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_S_2d", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_total_2d", meta_file, metadata)
+            elif shear_catalog_type == "metadetect":
+                copy(shear_tomo_file, "response", "tracers", "R", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_2d", meta_file, metadata)
+            elif shear_catalog_type == "lensfit":
+                copy(shear_tomo_file, "response", "tracers", "K", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "C", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "K_2d", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "C_2d", meta_file, metadata)
+            elif shear_catalog_type == "hsc":
+                copy(shear_tomo_file, "response", "tracers", "R", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "K", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "R_mean_2d", meta_file, metadata)
+                copy(shear_tomo_file, "response", "tracers", "K_2d", meta_file, metadata)
+
+            copy(shear_tomo_file, "tomography", "tracers", "N_eff", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "sigma_e", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "mean_e1", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "mean_e2", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "source_counts", meta_file, metadata)
+
+            copy(shear_tomo_file, "tomography", "tracers", "N_eff_2d", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "sigma_e_2d", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "mean_e1_2d", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "mean_e2_2d", meta_file, metadata)
+            copy(shear_tomo_file, "tomography", "tracers", "source_counts_2d", meta_file, metadata)
+
+            N_eff = shear_tomo_file["tomography/N_eff"][:]
+            N_eff_2d = shear_tomo_file["tomography/N_eff_2d"][:]
+            n_eff = N_eff / area_sq_arcmin
+            n_eff_2d = N_eff_2d / area_sq_arcmin
+
+            source_counts = shear_tomo_file["tomography/source_counts"][:]
+            source_counts_2d = shear_tomo_file["tomography/source_counts_2d"][:]
+
+            source_density = source_counts / area_sq_arcmin
+            source_density_2d = source_counts_2d / area_sq_arcmin
+
+            meta_file.create_dataset("tracers/n_eff", data=n_eff)
+            meta_file.create_dataset("tracers/source_density", data=source_density)
+            meta_file.create_dataset("tracers/source_density_2d", data=source_density_2d)
+
+            meta_file["tracers"].attrs["area"] = area
+            meta_file["tracers"].attrs["area_unit"] = "deg^2"
+            meta_file["tracers"].attrs["density_unit"] = "arcmin^{-2}"
+            metadata["n_eff"] = n_eff.tolist()
+            metadata["n_eff_2d"] = n_eff_2d.tolist()
+            metadata["source_density_2d"] = source_density_2d.tolist()
+            metadata["source_density"] = source_density.tolist()
+
+            copy_attrs(shear_tomo_file, "tomography", "tracers", meta_file, metadata)
+            
+
+    def copy_lens_metadata(self, meta_file, metadata, area, area_sq_arcmin):
+        if self.get_input("lens_tomography_catalog") == "none":
+            print("Skipping lens metadata")
+            return
+
+        with self.open_input("lens_tomography_catalog") as lens_tomo_file:
+
+            copy(lens_tomo_file, "tomography", "tracers", "lens_counts", meta_file, metadata)
+            copy(lens_tomo_file, "tomography", "tracers", "lens_counts_2d", meta_file, metadata)
+
+            lens_counts = lens_tomo_file["tomography/lens_counts"][:]
+            lens_counts_2d = lens_tomo_file["tomography/lens_counts_2d"][:]
+
+            lens_density = lens_counts / area_sq_arcmin
+            lens_density_2d = lens_counts_2d / area_sq_arcmin
+
+            meta_file.create_dataset("tracers/lens_density", data=lens_density)
+            meta_file.create_dataset("tracers/lens_density_2d", data=lens_density_2d)
+
+            metadata["lens_density"] = lens_density.tolist()
+            metadata["lens_density_2d"] = lens_density_2d.tolist()
+
+            copy_attrs(lens_tomo_file, "tomography", "tracers", meta_file, metadata)
+
     def run(self):
         # Read the area
         area = self.read_area()
-
-        shear_catalog_type = read_shear_catalog_type(self)
-
         area_sq_arcmin = area * 60**2
-        shear_tomo_file = self.open_input("shear_tomography_catalog")
-        lens_tomo_file = self.open_input("lens_tomography_catalog")
 
-        meta_file = self.open_output("tracer_metadata")
-        metadata = {}
-
-        def copy(tomo, in_section, out_section, name):
-            x = tomo[f"{in_section}/{name}"][:]
-            meta_file.create_dataset(f"{out_section}/{name}", data=x)
-            metadata[name] = x.tolist()
-
-        def copy_attrs(tomo, name, out_name):
-            for k, v in tomo[name].attrs.items():
-                meta_file[out_name].attrs[k] = v
-                if isinstance(v, np.ndarray):
-                    v = v.tolist()
-                elif isinstance(v, np.float64):
-                    v = float(v)
-                elif isinstance(v, np.int64):
-                    v = int(v)
-                metadata[k] = v
-
-        if shear_catalog_type == "metacal":
-            copy(shear_tomo_file, "response", "tracers", "R_gamma_mean")
-            copy(shear_tomo_file, "response", "tracers", "R_S")
-            copy(shear_tomo_file, "response", "tracers", "R_total")
-            copy(shear_tomo_file, "response", "tracers", "R_gamma_mean_2d")
-            copy(shear_tomo_file, "response", "tracers", "R_S_2d")
-            copy(shear_tomo_file, "response", "tracers", "R_total_2d")
-        elif shear_catalog_type == "metadetect":
-            copy(shear_tomo_file, "response", "tracers", "R")
-            copy(shear_tomo_file, "response", "tracers", "R_2d")
-        elif shear_catalog_type == "lensfit":
-            copy(shear_tomo_file, "response", "tracers", "K")
-            copy(shear_tomo_file, "response", "tracers", "C")
-            copy(shear_tomo_file, "response", "tracers", "K_2d")
-            copy(shear_tomo_file, "response", "tracers", "C_2d")
-        elif shear_catalog_type == "hsc":
-            copy(shear_tomo_file, "response", "tracers", "R")
-            copy(shear_tomo_file, "response", "tracers", "K")
-            copy(shear_tomo_file, "response", "tracers", "R_mean_2d")
-            copy(shear_tomo_file, "response", "tracers", "K_2d")
-
-        copy(shear_tomo_file, "tomography", "tracers", "N_eff")
-        copy(lens_tomo_file, "tomography", "tracers", "lens_counts")
-        copy(shear_tomo_file, "tomography", "tracers", "sigma_e")
-        copy(shear_tomo_file, "tomography", "tracers", "mean_e1")
-        copy(shear_tomo_file, "tomography", "tracers", "mean_e2")
-        copy(shear_tomo_file, "tomography", "tracers", "source_counts")
-
-        copy(shear_tomo_file, "tomography", "tracers", "N_eff_2d")
-        copy(lens_tomo_file, "tomography", "tracers", "lens_counts_2d")
-        copy(shear_tomo_file, "tomography", "tracers", "sigma_e_2d")
-        copy(shear_tomo_file, "tomography", "tracers", "mean_e1_2d")
-        copy(shear_tomo_file, "tomography", "tracers", "mean_e2_2d")
-        copy(shear_tomo_file, "tomography", "tracers", "source_counts_2d")
-
-        N_eff = shear_tomo_file["tomography/N_eff"][:]
-        N_eff_2d = shear_tomo_file["tomography/N_eff_2d"][:]
-        n_eff = N_eff / area_sq_arcmin
-        n_eff_2d = N_eff_2d / area_sq_arcmin
-
-        lens_counts = lens_tomo_file["tomography/lens_counts"][:]
-        source_counts = shear_tomo_file["tomography/source_counts"][:]
-        lens_counts_2d = lens_tomo_file["tomography/lens_counts_2d"][:]
-        source_counts_2d = shear_tomo_file["tomography/source_counts_2d"][:]
-
-        lens_density = lens_counts / area_sq_arcmin
-        source_density = source_counts / area_sq_arcmin
-        lens_density_2d = lens_counts_2d / area_sq_arcmin
-        source_density_2d = source_counts_2d / area_sq_arcmin
-
-        meta_file.create_dataset("tracers/n_eff", data=n_eff)
-        meta_file.create_dataset("tracers/lens_density", data=lens_density)
-        meta_file.create_dataset("tracers/source_density", data=source_density)
-        meta_file.create_dataset("tracers/lens_density_2d", data=lens_density_2d)
-        meta_file.create_dataset("tracers/source_density_2d", data=source_density_2d)
-        meta_file["tracers"].attrs["area"] = area
-        meta_file["tracers"].attrs["area_unit"] = "deg^2"
-        meta_file["tracers"].attrs["density_unit"] = "arcmin^{-2}"
-        metadata["n_eff"] = n_eff.tolist()
-        metadata["n_eff_2d"] = n_eff_2d.tolist()
-        metadata["lens_density"] = lens_density.tolist()
-        metadata["source_density"] = source_density.tolist()
-        metadata["lens_density_2d"] = lens_density_2d.tolist()
-        metadata["source_density_2d"] = source_density_2d.tolist()
-        metadata["area"] = area
-        metadata["area_unit"] = "deg^2"
-        metadata["density_unit"] = "arcmin^{-2}"
-        copy_attrs(shear_tomo_file, "tomography", "tracers")
-        copy_attrs(lens_tomo_file, "tomography", "tracers")
-        meta_file.close()
+        with self.open_output("tracer_metadata") as meta_file:
+            metadata = {}
+            metadata["area"] = area
+            metadata["area_unit"] = "deg^2"
+            metadata["density_unit"] = "arcmin^{-2}"
+            self.copy_source_metadata(meta_file, metadata, area, area_sq_arcmin)
+            self.copy_lens_metadata(meta_file, metadata, area, area_sq_arcmin)
 
         yaml_out = self.open_output("tracer_metadata_yml", wrapper=True)
         yaml_out.write(metadata)
         yaml_out.close()
 
-        shear_tomo_file.close()
-        lens_tomo_file.close()
 
     def read_area(self):
         with self.open_input("mask", wrapper=True) as f:
