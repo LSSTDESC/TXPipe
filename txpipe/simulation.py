5e7f24ebc4e70e64cfba045bd5ddb6dc4b398b10
elvinpoole <jack.elvinpoole@gmail.com>
2023-10-05 20:01:18 -0400

    added initial framework of glass

---
+++ txpipe/simulation.py
@@ -0,0 +1,46 @@
+from .base_stage import PipelineStage
+from .utils import choose_pixelization
+from .data_types import (
+    HDFFile,
+    ShearCatalog,
+    TextFile,
+    MapsFile,
+    FileCollection,
+    FiducialCosmology,
+    TomographyCatalog,
+)
+import glob
+import time
+
+class TXLogNormalGlass(PipelineStage):
+    """
+    Uses GLASS to generate a simulated catalog from lognormal fields
+    GLASS citation: 
+    https://ui.adsabs.harvard.edu/abs/2023OJAp....6E..11T
+    """
+
+    name = "TXLogNormalGlass"
+    parallel = False
+    inputs = [ 
+        ("mask", MapsFile),
+        ("lens_photoz_stack", HDFFile),
+        ("fiducial_cosmology", FiducialCosmology),
+    ]
+
+    outputs = [
+        ("binned_lens_catalog_unweighted", TomographyCatalog),  
+        ("lens_tomography_catalog_unweighted", TomographyCatalog), 
+        #TO DO: add shear maps to output
+    ]
+
+    config_options = {
+        "num_dens": None,
+    }
+
+    def run(self):
+        import glass.shells
+        import glass.fields
+        import glass.points
+        import glass.galaxies
+
+
