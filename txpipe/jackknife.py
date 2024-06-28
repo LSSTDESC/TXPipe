9ae914135091194bcb158b5174e7f047e9d6fab6
empEvil <emp160330@utdallas.edu>
2024-06-28 12:16:42 -0400

    Forcing jacknifeSources to assume HDF5

--- txpipe/jackknife.py
+++ txpipe/jackknife.py
@@ -110,6 +110,7 @@ class TXJackknifeCentersSource(TXJackknifeCenters):
             "dec_units": "degree",
             "every_nth": every_nth,
             "npatch": npatch,
+            "file_type": "HDF5",
         }
 
         # Create the catalog
