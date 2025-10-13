e9e32f49c859f1510eafb4af13ac1ae098de51a4
Joe Zuntz <joezuntz@googlemail.com>
2025-10-13 15:08:04 +0100

    Fix import and alignment

--- txpipe/extensions/__init__.py
+++ txpipe/extensions/__init__.py
@@ -1,5 +1,5 @@
 from .cluster_counts import *
 from .twopoint_cluster import TXTwoPointCluster
-from .twopoint_scia import TXSelfCalibrationIA
+from .twopoint_scia import TXTwoPointSelfCalibrationIA, TXTwoPointSourcePixels, TXTwoPointSCIAArc
 from .cluster_counts import CLClusterShearCatalogs, CLClusterBinningRedshiftRichness
 from .hos.fsb import HOSFSB
