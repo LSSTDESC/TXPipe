b12a236cca9129b91bf3a4cce43592f59a0f794e
empEvil <emp160330@utdallas.edu>
2025-11-03 16:21:02 +0000

    not needed anymore

--- txpipe/ingest/dp1_meta.py
+++
@@ -1,5 +0,0 @@
-from ..base_stage import PipelineStage
-from ..data_types import ShearCatalog, PhotometryCatalog, HDFFile, FileCollection
-from .lsst import process_photometry_data, process_shear_data
-import numpy as np
-
