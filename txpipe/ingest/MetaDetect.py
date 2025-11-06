c59725c15f051f9dc09ac99f01e54752a1417a62
empEvil <emp160330@utdallas.edu>
2025-11-06 13:01:30 +0000

    added options to just provide a parquet file

--- txpipe/ingest/MetaDetect.py
+++ txpipe/ingest/MetaDetect.py
@@ -5,7 +5,8 @@ from ceci.config import StageParameter
 from ..utils.hdf_tools import h5py_shorten, repack
 from ..utils.splitters import MetaDetectSplitter
 import numpy as np
-
+import os
+import pyarrow.parquet as pq
 
 class TXIngestMetaDetect(PipelineStage):
     """
@@ -30,7 +31,7 @@ class TXIngestMetaDetect(PipelineStage):
         "cosmology_tracts_only": StageParameter(bool, True, msg="Use only cosmology tracts."),
         "select_field": StageParameter(str, "", msg="Field to select (overrides cosmology_tracts_only)."),
         "collections": StageParameter(str, "LSSTComCam/DP1", msg="Butler collections to use."),
-        "file_path": StageParameter(str, "Nah", msg="if not using a Butler, you need to give a path to the file.")
+        "file_path": StageParameter(str, None, msg="if not using a Butler, you need to give a path to the file.")
     }
 
     def run(self):
@@ -111,7 +112,7 @@ class TXIngestMetaDetect(PipelineStage):
                     }
                 columns = list(shear_data["00"].keys())
                 splitter = MetaDetectSplitter(group, columns, variants)
-            
+
             for variant in ["00", "1p", "1m", "2p", "2m"]:
                 splitter.write_bin(shear_data[variant], variant)
             print(f"Processing chunk {i + 1} / {n_chunks}")
@@ -119,8 +120,42 @@ class TXIngestMetaDetect(PipelineStage):
         splitter.finish()
         shear_outfile.close()
 
-    def file_run():
-        print("Not implemented yet!")
+    def file_run(self):
+        file_path = self.config("file_path")
+        if file_path == None:
+            raise RuntimeError("You must either use a butler, or specify a file_path to your metadetect catalog.")
+
+        if not os.path.exists(file_path):
+            raise RuntimeError("No file where you said it would be.")
+
+        shear_outfile = self.open_output("shear_catalog")
+        group = shear_outfile.create_group("shear")
+        shear_outfile["shear"].attrs["catalog_type"] = "metadetect"
+
+        created_files = False
+        input_columns = self.get_input_columns()
+
+        chunk_size = self.config("chunk_rows")
+        pf = pq.ParquetFile(file_path)
+        for batch in pf.iter_batches(columns=input_columns, batch_size=chunk_size):
+            shear_data = process_metadetect_data(batch)
+            if not created_files:
+                created_files = True
+                variants = {
+                    "00": len(shear_data["00"]),
+                    "1p": len(shear_data["1p"]),
+                    "1m": len(shear_data["1m"]),
+                    "2p": len(shear_data["2p"]),
+                    "2m": len(shear_data["2m"]),
+                    }
+                columns = list(shear_data["00"].keys())
+                splitter = MetaDetectSplitter(group, columns, variants)
+
+            for variant in ["00", "1p", "1m", "2p", "2m"]:
+                splitter.write_bin(shear_data[variant], variant)
+
+        splitter.finish()
+        shear_outfile.close()
 
     def get_input_columns(self):
         input_columns = [
