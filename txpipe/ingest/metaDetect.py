51bd044988a58fb7325d1df8305e016c3e506f62
Eske M. Pedersen <emp160330@utdallas.edu>
2026-07-09 15:23:26 +0100

    Removed file_run

--- txpipe/ingest/metaDetect.py
+++ txpipe/ingest/metaDetect.py
@@ -27,7 +27,7 @@ class TXIngestRubinMetaDetect(PipelineStage):
         "use_butler": StageParameter(
                                      bool,
                                      True,
-                                     msg="Should be left on, unless you got an external file, in that case knock yourself out!"),
+                                     msg="We only support Butler run for now, so leave this on"),
         "butler_config_file": StageParameter(
             str, 
             "/global/cfs/cdirs/lsst/production/gen3/rubin/DP1/repo/butler.yaml",
@@ -37,14 +37,13 @@ class TXIngestRubinMetaDetect(PipelineStage):
         "select_field": StageParameter(str, "", msg="Field to select (overrides cosmology_tracts_only)."),
         "select_tracts": StageParameter(list, [], msg="list of tracts (overrides cosmology_tracts_only, but not select_field)."),
         "collections": StageParameter(str, "LSSTComCam/DP1", msg="Butler collections to use."),
-        "file_path": StageParameter(str, None, msg="if not using a Butler, you need to give a path to the file.")
-    }
+        }
 
     def run(self):
         if self.config["use_butler"]:
             self.butler_run()
         else:
-            self.file_run()
+            raise NotImplementedError("we only support Butler run for now")
 
         # Run h5repack on the file
         print("Repacking files")
@@ -126,45 +125,6 @@ class TXIngestRubinMetaDetect(PipelineStage):
             print(f"Processing chunk {i + 1} / {n_chunks}")
 
         splitter.finish()
-        shear_outfile.close()
-
-    def file_run(self):
-        file_path = self.config("file_path")
-        if file_path is None:
-            raise RuntimeError("You must either use a butler, or specify a file_path to your metadetect catalog.")
-
-        if not os.path.exists(file_path):
-            raise RuntimeError("No file where you said it would be.")
-
-        shear_outfile = self.open_output("shear_catalog")
-        group = shear_outfile.create_group("shear")
-        shear_outfile["shear"].attrs["catalog_type"] = "metadetect"
-
-        created_files = False
-        input_columns = self.get_input_columns()
-
-        chunk_size = self.config("chunk_rows")
-        pf = pq.ParquetFile(file_path)
-        for batch in pf.iter_batches(columns=input_columns, batch_size=chunk_size):
-            shear_data = process_metadetect_data(batch)
-            
-            if not created_files:
-                created_files = True
-                variants = {
-                    "ns": len(shear_data["ns"]),
-                    "1p": len(shear_data["1p"]),
-                    "1m": len(shear_data["1m"]),
-                    "2p": len(shear_data["2p"]),
-                    "2m": len(shear_data["2m"]),
-                    }
-                columns = list(shear_data["ns"].keys())
-                splitter = MetaDetectSplitter(group, columns, variants)
-
-            for variant in META_VARIANTS:
-                data = sanitize(shear_data[variant])
-                splitter.write_bin(data, variant)
-        if created_files:
-            splitter.finish()
         shear_outfile.close()
 
     def get_input_columns(self):
