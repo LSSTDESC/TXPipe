0eab8841c7adcca58fb0041748d688aec6cba350
Joe Zuntz <joezuntz@googlemail.com>
2026-07-16 10:39:21 +0100

    Remove stub code

--- txpipe/ingest/dp1.py
+++ txpipe/ingest/dp1.py
@@ -334,10 +334,6 @@ class TXIngestDataPreview1(PipelineStage):
         return n
 
 
-class TXIngestDataPreview2(TXIngestDataPreview1):
-    name = "TXIngestDataPreview2"
-    
-
 def sanitize(data):
     """
     Convert unicode arrays into types that h5py can save
