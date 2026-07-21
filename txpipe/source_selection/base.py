531b24eb34c4432e0a65a46b3aeaf5de71673e0c
myamamoto26 <masaya.yamamoto@princeton.edu>
2026-07-21 11:12:59 -0400

    moved mag/color cuts to dp2.py

--- txpipe/source_selection/base.py
+++ txpipe/source_selection/base.py
@@ -335,13 +335,6 @@ def select_weak_lensing_sample(data, config, calling_from_select=False):
     # output is different in each case
     s2n_cut = config["s2n_cut"]
     T_cut = config["T_cut"]
-    mag_g_cut = config["mag_g_cut"]
-    mag_r_cut = config["mag_r_cut"]
-    mag_i_cut = config["mag_i_cut"]
-    mag_z_cut = config["mag_z_cut"]
-    gmr_cut = config["g-r_cut"]
-    rmi_cut = config["r-i_cut"]
-    imz_cut = config["i-z_cut"]
 
     verbose = config["verbose"]
     variant = data.suffix
@@ -366,20 +359,10 @@ def select_weak_lensing_sample(data, config, calling_from_select=False):
     sel &= s2n > s2n_cut
     f3 = sel.sum() / n0
 
-    # We should also have some crazy color cuts and magnitude cuts which should come from PZ group
-    sel &= (data["mag_g"] < mag_g_cut) & \
-        (data["mag_r"] < mag_r_cut) & \
-        (data["mag_i"] < mag_i_cut) & \
-        (data["mag_z"] < mag_z_cut) & \
-        (np.abs(data["mag_g"] - data["mag_r"]) < gmr_cut) & \
-        (np.abs(data["mag_r"] - data["mag_i"]) < rmi_cut) & \
-        (np.abs(data["mag_i"] - data["mag_z"]) < imz_cut)
-    f4 = sel.sum() / n0
-
     # Finally we want objects that have been put into any of our other
     # tomographic bins
     sel &= data["zbin"] >= 0
-    f5 = sel.sum() / n0
+    f4 = sel.sum() / n0
 
     # Print out a message.  If we are selecting a 2D sample
     # this is the complete message.  Otherwise if we are about
@@ -387,10 +370,10 @@ def select_weak_lensing_sample(data, config, calling_from_select=False):
     # as above
     if verbose and calling_from_select:
         print(
-            f"Tomo selection ({variant}) {f1:.2%} flag, {f2:.2%} size, {f3:.2%} SNR, {f4:.2%} mag/color",
+            f"Tomo selection ({variant}) {f1:.2%} flag, {f2:.2%} size, {f3:.2%} SNR",
             end="",
         )
     elif verbose:
-        print(f"2D selection ({variant}) {f1:.2%} flag, {f2:.2%} size, {f3:.2%} SNR, {f4:.2%} mag/color, {f5:.2%} any z bin")
+        print(f"2D selection ({variant}) {f1:.2%} flag, {f2:.2%} size, {f3:.2%} SNR, {f4:.2%} any z bin")
         print("total 2D", sel.sum())
     return sel
--- txpipe/source_selection/dp2.py
+++ txpipe/source_selection/dp2.py
@@ -1,7 +1,7 @@
 from .metadetect import TXSourceSelectorMetadetect
 from .base import select_weak_lensing_sample
 from ..shear_calibration import MetaDetectCalculator
-
+import numpy as np
 
 class TXSourceSelectorMetadetectDP2(TXSourceSelectorMetadetect):
     """
@@ -43,6 +43,23 @@ def select_weak_lensing_sample_metadetect_dp2(data, config, calling_from_select=
     n0 = sel.size
 
     # --- metadetect-specific cuts go here ---
+    mag_g_cut = config["mag_g_cut"]
+    mag_r_cut = config["mag_r_cut"]
+    mag_i_cut = config["mag_i_cut"]
+    mag_z_cut = config["mag_z_cut"]
+    gmr_cut = config["g-r_cut"]
+    rmi_cut = config["r-i_cut"]
+    imz_cut = config["i-z_cut"]
+
+    # We should also have some crazy color cuts and magnitude cuts which should come from PZ group
+    sel &= (data["mag_g"] < mag_g_cut) & \
+        (data["mag_r"] < mag_r_cut) & \
+        (data["mag_i"] < mag_i_cut) & \
+        (data["mag_z"] < mag_z_cut) & \
+        (np.abs(data["mag_g"] - data["mag_r"]) < gmr_cut) & \
+        (np.abs(data["mag_r"] - data["mag_i"]) < rmi_cut) & \
+        (np.abs(data["mag_i"] - data["mag_z"]) < imz_cut)
+
     # Follow the same pattern as select_weak_lensing_sample, but add extra cuts for metadetect catalogs.:
     mfrac_cut = config["mfrac_cut"]
     mfrac = data["object_mask_fraction"]
