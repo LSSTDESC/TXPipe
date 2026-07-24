7d47fc8cbfa8bc2c1002d6098c27d56b41532218
Joe Zuntz <joezuntz@googlemail.com>
2026-07-24 11:45:09 +0200

    WIP on selection using RAIL

--- txpipe/source_selection/dp2.py
+++ txpipe/source_selection/dp2.py
@@ -1,5 +1,6 @@
 from .metadetect import TXSourceSelectorMetadetect
 from .base import select_weak_lensing_sample, TXSourceSelectorBase
+from ..data_types import ShearCatalog, HDFFile
 from ..shear_calibration import metadetect_variants, MetaDetectCalculator, band_variants, META_VARIANTS
 from ceci.config import StageParameter
 import numpy as np
@@ -63,6 +64,170 @@ class TXSourceSelectorMetadetectDP2(TXSourceSelectorMetadetect):
         ]
         calculators.append(MetaDetectCalculator(select_weak_lensing_sample_metadetect_dp2, delta_gamma))
         return calculators
+
+
+
+class TXDP2BasicSelectionMetadetect(PipelineStage):
+    """Make a cut catalog applying the underlying DP2 selection without any tomograpy.
+
+    RAIL's SOMPZ approach has a slightly different sequence of calculations to what we
+    have so far been assuming in TXPipe. In that model, RAIL's SOMPZ stages want to combine
+    tomography and n(z) calculation, so we need to supply it a catalog that has already
+    been cut down to just the core (SNR, size, etc) selection used for the WL sample.
+
+    So our strategy here is to generate a stripped-down catalog to pass to SOMPZ, but also
+    to save the selection so that later we can correctly do the shear selection bias calibration.
+
+    Hopefully in DR1 we can improve the integration.
+    """
+
+    inputs = [
+        ("shear_catalog", ShearCatalog),
+    ]
+    outputs = [
+        ("cut_shear_catalog", ShearCatalog),
+        ("base_shear_selection_catalog", HDFFile),
+    ]
+
+    config_options = TXSourceSelectorMetadetectDP2.config_options.copy()
+
+    def run(self):
+        
+        input_file = self.open_input("shear_catalog")
+        output_file = self.open_output("cut_shear_catalog", parallel=True)
+        selection_file = self.open_output("base_shear_selection_catalog", parallel=True)
+        output_group, selection_group = self.setup_output(input_file, output_file, selection_file)
+        
+
+        # We made full-length catalogs for the output file above, but of course
+        # they will be smaller really because we will throw away some data.
+        # But we don't know how small until we do it, so for now we will just
+        # keep track of the sizes of all the outputs for each variant        
+        final_sizes = np.zeros(len(META_VARIANTS), dtype=int)
+        # Each process selects and writes a different variant, so at least
+        # we can use up to 5 parallel processes
+        for variant in self.split_tasks_by_rank(META_VARIANTS):
+            index = 0
+            output_subgroup = output_group[variant]
+            # Read a chunk of data
+            for (s, e, data) in self.data_iterator(input_file, variant):
+
+                # Run the standard selection function
+                sel = select_tomographic_weak_lensing_sample_metadetect_dp2(data, self.config)
+
+                # Save the selection mask
+                selection_group[f"select_{variant}"][s:e] = sel
+                
+                # output just the selected part to the new catalog.
+                # It's not clear we actually want to save all the columns here
+                # the content that is needed is what RAIL needs to do PZ.
+                # I think that's just magnitudes, weight, ra, dec (for the clustering PZ)
+                # so really I should cut that down here, especially when
+                # we have the larger DP2 catalogs. That's a TODO.
+                n = sel.sum()
+                for name, col in data.items():
+                    output_subgroup[name][index: index + n] = col
+                index += n
+            # Record the size of the selected data so we can resize later
+            final_sizes[META_VARIANTS.index(variant)] = index
+        
+        # resize all the columns
+        self.resize_all_columns(output_group, final_sizes)
+
+    def setup_output(self, input_file, output_file, selection_file):
+        output_group = output_file.create_group("shear")
+        g = f.create_group("shear")
+
+        # Create a group for each variant (1p, 1m, etc.)
+        sizes = {}
+        for variant in META_VARIANTS:
+            input_subgroup = input_file[f"shear/{variant}"]
+            output_subgroup = output_group.create_group(variant)
+            # and in each of those groups create a copy of all the columns.
+            # We make it the same size as the input column and cut it down at the end.
+            size = input_subgroup['ra'].size
+            sizes[variant] = size
+            for col in input_subgroup.keys():
+                if not col.size == size:
+                    raise ValueError("Columns in shear cat are different sizes")
+                output_subgroup.create_dataset(name=name, dtype=col.dtype, shape=col.shape, maxshape=(None,))
+
+        # We will also save the boolean selection function so that later on we can combine
+        # this with the tomographic bin selection that RAIL will return
+        selection_group = selection_file.create_group("selection")
+        for variant in META_VARIANTS:
+            size = sizes[varant]
+            selection_group.create_dataset(f"select_{variant}", dtype=bool, shape=(size,))
+        
+        return output_group, selection_group
+
+        
+    # resize each one
+    def resize_all_columns(self, output_group, final_sizes):
+        # each of the proceses does a different variant, and the array of final
+        # sizes is by variant, so it should be zero for the entries corresponding
+        # to the other processes, and so summing in-place should work fine.
+        in_place_reduce(final_sizes, self.comm, allreduce=True)
+        # The reason we have to share the final sizes with all the processes is that
+        # operations that change the structure of the HDF5 file must be done collectively
+        # by all processes, and I think that includes resizing.
+        for v, variant in enumerate(META_VARIANTS):
+            output_subgroup = output_group.create_group(variant)
+            size = final_sizes[v]
+            for col in output_subgroup.keys():
+                output_subgroup[col].resize(size)
+
+
+    def data_iterator(self, input_file, variant):
+        chunk_size = self.config["chunk_rows"]
+        group = input_file["shear/" + variant]
+        index = 0
+        size = group["ra"].size
+        while index < size:
+            s = index
+            e = index + chunk_size
+            data = {name: col[s:e] for name, col in group.items()}
+            s = e
+
+class TXDP2CombineBasicAndRailSelections(TXSourceSelectorMetadetect):
+    """
+    Combine the selection mask from TXDP2BasicSelectionMetadetect and
+    the tomographic selection from RAIL into a single metadetect selection
+    process. We will need an anacal version of this too.
+    """
+    inputs = [
+        ("rail_bhat_thing_rename_me", HDFFile),
+        ("base_shear_selection_catalog", HDFFile),
+    ]
+    outputs = [("shear_tomography_catalog", TomographyCatalog)]
+
+    def setup_response_calculators(self, nbin_source):
+        delta_gamma = self.config["delta_gamma"]
+        calculators = [
+            MetaDetectCalculator(self.select, delta_gamma)
+            for i in range(nbin_source)
+        ]
+        calculators.append(MetaDetectCalculator(self.select_2d, delta_gamma))
+        return calculators
+
+
+    def data_iterator(self):
+        # As above, this is where we work out which columns we need.
+        chunk_rows = self.config["chunk_rows"]
+        for variant in META_VARIANTS:
+            # we need to iterate both the mask we saved in TXDP2BasicSelectionMetadetect
+            # and the tomographic bin choices that RAIL made for us.
+            it1 = self.iterate_hdf()
+            it2 = self.iterate_hdf()
+    
+    def select(self, data, config, bin_index):
+        sel = (data["bin"] == bin_index) & (data["base_selection"] == True)
+
+    def select_2d(self, data, config):
+        pass
+
+    
+
 
 
 def select_weak_lensing_sample_metadetect_dp2(data, config, calling_from_select=False):
