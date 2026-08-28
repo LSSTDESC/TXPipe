fff5ee41191db361f4990832fca6a9f3a92959da
Thomas Cornish <tomcornish1996@gmail.com>
2026-08-28 10:10:09 +0100

    Decided that recomputing degrees of freedom each time was best in case the number of bins changes

--- txpipe/lssweights.py
+++ txpipe/lssweights.py
@@ -821,10 +821,6 @@ class TXLSSDensitySkyCuts(TXLSSDensityNullTests):
         nsysmaps = len(self.sys_maps)
         print(f'Sys maps: {self.sys_names}')
 
-        # Degrees of freedom for reduced chi^2 calculation; for a multilinear fit to N SP maps with M bins
-        # each, this is taken as dof = N * M - (N + 1), where the +1 is for the intercept term
-        dof = nsysmaps * nsysbins - nsysmaps - 1
-
         # Construct mask for each tomographic bin; the final mask will be a intersection of all of them
         mask_inter = []
         results = []
@@ -890,6 +886,10 @@ class TXLSSDensitySkyCuts(TXLSSDensityNullTests):
                 ATCy = A.T @ icov @ density_corrs.ndens
                 alphas = np.linalg.solve(ATCA, ATCy)
 
+                # Degrees of freedom for reduced chi^2 calculation; this can change with each
+                # iteration if the number of bins decreases (possible if many pixels in the SP
+                # map have similar values)
+                dof = A.shape[0] - A.shape[1]
                 # add model predictions and compute reduced chi^2
                 ndens_pred = density_corrs.linear_model(alphas)
                 density_corrs.add_model(ndens_pred, "multilinear")
