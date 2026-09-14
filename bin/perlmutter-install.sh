387712d467aa603ceda9388a9e3e375afd880d28
joezuntz <joezuntz@googlemail.com>
2026-09-14 03:24:19 -0700

    Firecrown 1.15

--- bin/perlmutter-install.sh
+++ bin/perlmutter-install.sh
@@ -14,12 +14,12 @@ mamba activate ./conda
 FIRECROWN_DIR=firecrown-tmp-${RANDOM}
 
 # TODO: check if this is still needed.
-git clone --branch v1.14.0 https://github.com/LSSTDESC/firecrown  ${FIRECROWN_DIR}
-cd ${FIRECROWN_DIR}
-sed -i '/numcosmo/d' setup.cfg
-pip install .
-cd ..
-rm -rf ${FIRECROWN_DIR}
+#git clone --branch v1.14.0 https://github.com/LSSTDESC/firecrown  ${FIRECROWN_DIR}
+#cd ${FIRECROWN_DIR}
+#sed -i '/numcosmo/d' setup.cfg
+#pip install .
+#cd ..
+#rm -rf ${FIRECROWN_DIR}
 
 # Now we remove the installed mpi4py version and replace it with our own.
 # We also remove libfabric which is installed as a dependency of mpich
